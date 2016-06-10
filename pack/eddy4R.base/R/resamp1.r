25a1bb9ccc9be2e71d40bd20f36e0bf495fca8d7
NDurden <ndurden@neoninc.org>
2016-06-10 13:18:04 -0600

    Ongoing! Do not merge.

--- pack/eddy4R.base/R/resamp1.r
+++ pack/eddy4R.base/R/resamp1.r
@@ -1,12 +1,17 @@
 ##############################################################################################
-#' @title Resampling using zoo() function
+#' @title Resampling function
 
 #' @author Stefan Metzger \email{eddy4R.info@gmail.com}
 #' Natchaya Pingintha-Durden
 
-#' @description Resampling using zoo() function.
+#' @description Function definition. Resampling data by changing the sample rate from high frequency to low frequency.
 
-#' @param Currently none
+#' @param \code{data} A vector or matrix of type numeric, containing the data to be converted
+#' @param \code{FreqInp} Input sampling or measurements frequency. Of class "numeric" or "integer". [Hz]
+#' @param \code{FreqOut} Desired out put frequency. Of class "numeric" or "integer". [Hz]
+#' @param \code{MethResp} An object of class "character" containing the resampling method ("zoo","filt"). [-]
+#' @param \code{Col} Specific column in \code{data} which containing the discontinuity variable e.g. azimuth angle of wind direction. Defaults to NULL. [-] 
+
 
 #' @return Currently none
 
@@ -30,37 +35,97 @@
 #     Initail naming convention for eddy4R
 ##############################################################################################
 
-resamp1<-function(mat.raw,freq,freq_res,EIDAS) {
+#resamp1<-function(mat.raw,freq,freq_res,EIDAS) {#
+def.resp <- function (data, FreqInp, FreqOut, MethResp, Col=NULL) {
 #--------------------------------------------------------------------------------------------
 #RESAMPLING
   #creating cartesian vector angles from azimuth angles
 #mat.raw<-rawdata
-  if(EIDAS == T) {
-    mat.raw<-cbind(mat.raw,def.conv.az.cart(mat.raw[,22]))	#heading nacelle
-    mat.raw<-cbind(mat.raw,def.conv.az.cart(mat.raw[,38]))	#heading TCM
+  #if(EIDAS == T) {#
+    #mat.raw<-cbind(mat.raw,def.conv.az.cart(mat.raw[,22]))	#heading nacelle#
+    #mat.raw<-cbind(mat.raw,def.conv.az.cart(mat.raw[,38]))	#heading TCM#
+  #}#
+if (!is.null(Col)){
+  #creating a new data frame for the discontinuity variables
+  colData <-data[Col]
+  temp01 <-c()
+  
+  #converting the discontinuity variables from azimuth angles to cartesian vector angles
+  for (i in 1:length(Col)){
+    
+    temp01[[i]]<- def.conv.az.cart(colData[,i]) 
+    
   }
+  data <- cbind(data,temp01)
+}
 
   #calculation of control variables
-  avg_no=freq/freq_res			#number of datasets to be averaged over
-  rows=as.integer(nrow(mat.raw)/avg_no)	#number of rows of the output data
-
-  #allocate matrix
-  mat.raw.res=matrix(nrow=rows, ncol=ncol(mat.raw),
-    dimnames = list(c(as.character(1:rows)),dimnames(mat.raw) [[2]]))
+  #avg_no=freq/freq_res			#number of datasets to be averaged over#
+  NumSamp = FreqInp/FreqOut #number of datasets to be averaged over
+  #rows=as.integer(nrow(mat.raw)/avg_no)	#number of rows of the output data#
+  NumRow = as.integer(nrow(data)/NumSamp) #number of rows of the output data
+  
+#allocate matrix
+ # mat.raw.res=matrix(nrow=rows, ncol=ncol(mat.raw),#
+  #  dimnames = list(c(as.character(1:rows)),dimnames(mat.raw) [[2]])) #
 
   #actual resampling
-  for (i in 1:ncol(mat.raw)){
-    mat.raw.res[,i]<-zoo::rollapply(zoo(mat.raw[,i]),avg_no,mean,na.rm=T,by=avg_no)
+  #for (i in 1:ncol(mat.raw)){#
+   # mat.raw.res[,i]<-zoo::rollapply(zoo(mat.raw[,i]),avg_no,mean,na.rm=T,by=avg_no)#
+  #}#
+
+#resampling method1 by using rollapply in zoo
+if (MethResp == "zoo"){
+  
+  for (i in 1:ncol(data)){
+    
+    temp02 <- zoo::rollapply(zoo::zoo(data[,i]), NumSamp, mean, na.rm=T, by=NumSamp)
+    if(i == 1) temp03 <- temp02 else temp03 <- cbind(temp03, temp02)
   }
+}
+
+#resampling method1 by using filter() function 
+if (MethResp == "filt"){
+  
+  for(i in 1:ncol(data)) {
+    temp02 <- stats::filter(data[,i], rep(1 / NumSamp, NumSamp), sides=2)
+    if(i == 1) temp03 <- temp02 else temp03 <- cbind(temp03, temp02)
+  }  
+  
+}
+
+dimnames(temp03)[[2]] <- dimnames(data)[[2]]
+
+#discard non-used data intervals
+whr <- seq(ceiling(NumSamp), nrow(temp03), by=NumSamp)
+rpt <- temp03[whr,]
+
+
 
   #overwriting the azimuth angle averages with the re-calculation from cartesian vector averages
-  if(EIDAS == T) {
-    mat.raw.res[,22]<-def.conv.cart.az(mat.raw.res[,(ncol(mat.raw)-3):(ncol(mat.raw)-2)])	#heading nacelle
-    mat.raw.res[,38]<-def.conv.cart.az(mat.raw.res[,(ncol(mat.raw)-1):(ncol(mat.raw)-0)])	#heading TCM
-    mat.raw.res<-mat.raw.res[,1:(ncol(mat.raw)-4)]
-  }
+  #if(EIDAS == T) {#
+    #mat.raw.res[,22]<-def.conv.cart.az(mat.raw.res[,(ncol(mat.raw)-3):(ncol(mat.raw)-2)])	#heading nacelle#
+   # mat.raw.res[,38]<-def.conv.cart.az(mat.raw.res[,(ncol(mat.raw)-1):(ncol(mat.raw)-0)])	#heading TCM #
+  #  mat.raw.res<-mat.raw.res[,1:(ncol(mat.raw)-4)] #
+  #}#
+if (!is.null(Col)){
+  
+  for (i in 1:length(Col)){
+  
+  j <- 2*(length(Col))
+  m <- ncol(rpt)-j
+  n <- 2*i-1
+  
+  col01 <- (m+n) #indicated 1st column of cartesian vector variable that willbe used to calculate the azimuth angle
+  col02 <- (m+n+1) #indicated 2nd column of cartesian vector variable that willbe used to calculate the azimuth angle
+  
+  rpt[,Col[i]] <- def.conv.cart.az(rpt[,col01:col02])  
+}
+rpt <- rpt[,1:(2*(length(Col)))]  #delete
+}
 
-return(mat.raw.res)
+
+return(data.frame(rpt))
 }
 
 
