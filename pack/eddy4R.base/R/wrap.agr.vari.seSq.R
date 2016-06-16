1d2f5be556bbaa076110021364918b1b2b321379
kexu2014 <xuke2012abroad@gmail.com>
2016-06-16 12:01:35 -0500

    make 3 changes:
    1. Describe explictly that the input data can be irregular, because data expansion is included in the current wrapper to generate unbiased mean, variance and squared standard error.
    
    2. add explanation and example for "zone".
    
    3. change internal index "xx" into "x".

--- pack/eddy4R.base/R/wrap.agr.vari.seSq.R
+++ pack/eddy4R.base/R/wrap.agr.vari.seSq.R
@@ -5,7 +5,7 @@
 #' Ke Xu \email{xuke2012abroad@gmail.com} \cr
 
 #' @description 
-#' Function wrapper. Calculate aggregated variance and squared standard error from a finer to a coarser temporal resolution: from minutely to hourly resolution, from hourly to diurnal cycle, and from diurnal cycle to monthly
+#' Function wrapper. Calculate aggregated variance and squared standard error from a finer to a coarser temporal resolution: from minutely to hourly resolution, from hourly to diurnal cycle, and from diurnal cycle to monthly. The input data can be irregular, because data expansion is included in the current wrapper to generate unbiased mean, variance and squared standard error. 
 
 
 #' @param \code{data} Dataframe of type numeric containing column vectors \code{DOY}, \code{mean}, \code{vari}, and \code{seSq} of equal length.
@@ -13,7 +13,7 @@
 #' @param \code{mean} Vector of type numeric. Means of the variable of interest at finer resolution, e.g. minutely [user-defined].
 #' @param \code{vari} Vector of type numeric. Variances of the variable of interest at finer resolution, e.g. minutely [user-defined^2].
 #' @param \code{seSq} squared standard error of the scalar at minutely resolution, e.g. minutely square of standard error of sensible heat. [square of unit of scalar, e.g. (W m-2)^2].
-#' @param \code{zone} The time zone of the location [unitless].
+#' @param \code{zone} The time zone of the location, which is the offset from Coordinated Universal Time (UTC) by a whole number of hours. If local time is ahead (behind) the Greenwich mean time, zone is a positive (negative) number. e.g. CST" [hour]. a time offset in Wisconsin, in the North American Central Time Zone, would be -6.
 #' @param \code{MethAgr} String type. One of three choices: "hrly", "mdc", "moly". "hrly" represents aggregation from minutely to hourly resolution; "mdc" represents aggregation from hourly to diurnal cycle; "moly" represents aggregation from diurnal cycle to monthly resolution.
 
 #' @return Returns object of class "dataframe": if MethAgr is "hrly", the dataframe is [n, 1:5] containing date, hour, mean, variance, square of standard error of the scalar at hourly resolution
@@ -33,7 +33,7 @@
 #'   vari = rnorm(100)^2,   
 #'   seSq = rnorm(100)^2
 #' ), 
-#' zone = 6,
+#' zone = -6,
 #' MethAgr = "hrly"
 #' )
 
@@ -74,7 +74,7 @@ wrap.agr.vari.seSq <- function(
   if (MethAgr == "hrly" | MethAgr == "mdc"){
     
     #translate from UTC to local time
-    data$DOY <- data$DOY - zone/24   
+    data$DOY <- data$DOY + zone/24   
     data$date <- floor(data$DOY) 
     #translate from UTC to local time
     data$hour <- (data$DOY - floor(data$DOY))*24 
@@ -83,7 +83,7 @@ wrap.agr.vari.seSq <- function(
   
   #prepare idx for sapply function
   if (MethAgr == "hrly"){
-    idx <- base::as.vector(base::sapply(base::unique(data$date), function(xx) (xx + c(0:23)/24)))
+    idx <- base::as.vector(base::sapply(base::unique(data$date), function(x) (x + c(0:23)/24)))
     hour <- 24 * (idx-floor(idx))
     date <- floor(idx)
   }
@@ -95,11 +95,11 @@ wrap.agr.vari.seSq <- function(
   
   # calculate aggregated mean, variance, squared standard error
   tmp <- list()
-  tmp <- sapply(1:length(idx), function(xx) {
+  tmp <- sapply(1:length(idx), function(x) {
     
-    if (MethAgr == "hrly") whr <- which(abs(floor(data$hour) - hour[xx]) < 0.1 & abs(data$date - date[xx]) < 0.1)
+    if (MethAgr == "hrly") whr <- which(abs(floor(data$hour) - hour[x]) < 0.1 & abs(data$date - date[x]) < 0.1)
     
-    if (MethAgr == "mdc") whr <- which(abs(data$hour - (xx-1)) < 0.1)
+    if (MethAgr == "mdc") whr <- which(abs(data$hour - (x-1)) < 0.1)
     
     if (MethAgr == "moly") whr <- c(1:length(data$mean))
     
