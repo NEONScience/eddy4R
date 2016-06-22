664e6a0900aa5de9966d71a5034b5385e406921b
hongyanluo <hluo@neoninc.org>
2016-06-22 09:29:13 -0600

    Edit Header

--- pack/eddy4R.base/R/RBPCdet.r
+++ pack/eddy4R.base/R/RBPCdet.r
@@ -1,34 +1,46 @@
 ##############################################################################################
 #' @title RMSD, bias, precision and coefficient of determination - incl. deadband
 
-# type (one of function defintion, function wrapper, workflow, demo): function defintion
+#' @author Stefan Metzger \email{eddy4R.info@gmail.com} \cr
+#' @author Hongyan Luo \email{eddy4R.info@gmail.com}
 
-# license: Terms of use of the NEON FIU algorithm repository dated 2015-01-16
+#' @description Function defintion.RMSD, bias, precision and coefficient of determination - incl. deadband.  
 
-#' @author Stefan Metzger \email{eddy4R.info@gmail.com}
+#' @param \code{refe}  Variable of class numeric. Reference data.Same unit as test data. 
+#' @param \code{test}  Variable of class numeric. Test data.Same unit as reference data.
+#' @param \code{perc}  Variable of class logical. It describe if the output is in percentage.
+#' @param \code{deba}  Variable of class numeric. Numbers of the deadband around zero denominator
+#' @param \code{debaRltv}  Variable of class logical.Absolute or relative (percentage) deadband around zero. True indicates deba in relative (percentage), False indicates deba in absolute numbers. 
+#' 
+
+#' @return Currently none
+
+#' @references Currently none
+#' license: Terms of use of the NEON FIU algorithm repository dated 2015-01-16.
+
+
+#' @keywords RMSD, bias, precision, R-square
+
+#' @examples 
+#' #refe dataset
+#' df1 <- c(runif(200, min=0, max=360))
+#' #test dataset
+#' df2 <- c(runif(200, min=0, max=360))
+#' #run function
+#' def.rmsd.diff.prcs.rsq(refe=df1,test=df2,perc = FALSE,deba=NULL, debaRltv=FALSE)
+
+#' @seealso Currently none
+
+#' @export
 
 # changelog and author contributions / copyrights
 #   Stefan Metzger (2014-11-19)
 #     original creation
 #   Stefan Metzger (2015-11-28)
 #     re-formualtion as function() to allow packaging
-
-#' @description RMSD, bias, precision and coefficient of determination - incl. deadband.
-
-#' @param Currently none
-
-#' @return Currently none
-
-#' @references Currently none
-
-#' @keywords Currently none
-
-#' @examples Currently none
-
-#' @seealso Currently none
-
-#' @export
-##############################################################################################
+#   Hongyan Luo (2016-06-10)
+#     adjust to eddy4R terms
+#############################################################################################
 
 #RETURNS RESIDUAL STANDARD ERROR AND COEFFICIENT OF DETERMINATION FOR TWO DATA SERIES
 
