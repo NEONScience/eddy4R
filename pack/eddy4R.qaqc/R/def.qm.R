a96226bd85515a8dc311d8a3c0f05ab33f616743
NDurden <ndurden@battelleecology.org>
2016-09-09 12:54:44 -0600

    Modified def.qf.finl.l1() to def.qm() which only calculate quality metric.

--- pack/eddy4R.qaqc/R/def.qm.R
+++ pack/eddy4R.qaqc/R/def.qm.R
@@ -1,32 +1,31 @@
 ##############################################################################################
-#' @title Final Quality Flag (basic L1 data products) 
+#' @title Quality Metrics 
 
 #' @author
 #' Cove Sturtevant \email{csturtevant@neoninc.org} \cr
 #' Natchaya Pingintha-Durden \email{ndurdent@neoninc.org} \cr
 
 #' @description 
-#' Function definition. Determine the final quality flag for individual level 1 data product following the method described in Smith et.al. (2014). The quality metrics of failed, pass, and NA for each of the individual quality flag, alpha and beta quality metric were also computed in this function.
+#' Function definition. Determine the quality metrics of failed, pass, and NA for each of the individual quality flag following the method described in Smith et.al. (2014).
 
 #' @param \code{data} A dataframe containing the input flag data. Of class integer". [-] 
 #' @param \code{nameFlag} A vector of class "character" containing the name of flag data which will be used to calculate quality metrics of failed, pass, NA, alpha, and beta and final quality flag. [-] 
-#' @param \code{rtioAlphBeta} Ratio of alpha quailty metric to beta quailty metric for determing the final quality flag. Default to rtioAlphBeta=c(2,1). [-] 
-#' @param \code{thsh} Threshold for determine the condition (pass = 0 or failed = 1) of final quality flag. Default to 20 percent. [percent]
 
-#' @return A dataframe containing quality metrics of failed, pass, and NA for each of the individual flag defined in \code{nameFlag} as well as alpha and beta quality metric and final qualiy flag.
+#' @return A dataframe containing quality metrics of failed, pass, and NA for each of the individual flag defined in \code{nameFlag}.
 
 #' @references 
 #' NEON Algorithm Theoretical Basis Document: Quality Flags and Quality Metrics for TIS Data Products (NEON.DOC.001113) \cr
 #' Smith, D.E., Metzger, S., and Taylor, J.R.: A transparent and transferable framework for tracking quality information in large datasets. PLoS ONE, 9(11), e112249.doi:10.1371/journal.pone.0112249, 2014. \cr 
 
-#' @keywords NEON QAQC, quality flags and metrics, final quality flag
+#' @keywords NEON QAQC, quality flags, quality metrics
 
 #' @examples
 #' qfA <- c(0,0,0,0,0,1,1,1,1,1,0,0,0,0,1) 
 #' qfB <- c(0,0,-1,-1,-1,1,1,0,0,0,0,0,0,0,0)
 #' qfC <- c(0,1,1,0,0,0,0,0,0,0,0,0,-1,-1,-1)
-#' data<- data.frame(qfA,qfB,qfC)
-#' test<- def.qf.finl.l1(data=data, nameFlag=colnames(data), rtioAlphBeta=c(2,1), thsh=20)
+#' test<-list()
+#' test$qf <- data.frame(qfA,qfB,qfC)
+#' test$qm<- def.qm(data=test$qf, nameFlag=colnames(test$qf))
 
 #' @seealso def.qfqm.l1()
 
@@ -38,15 +37,14 @@
 #   Natchaya P-Durden (2016-08-15)
 #     modified def.qfqm.l1()
 ##############################################################################################
-def.qf.finl.l1 <- function (
+def.qm <- function (
   data,
-  nameFlag,
-  rtioAlphBeta=c(2,1),
-  thsh=20) {
+  nameFlag
+  ) {
   
   # Take inventory of the flags we have and set up output naming
   #nameFlag <- colnames(data) # name of flags
-  qfqm <- as.list(data) # copy variable names to output
+  qm <- as.list(data) # copy variable names to output
   numFlag <- length(nameFlag) #number of flags 
   nameVarOut <- c() #output variable names
   nameQm <- c("Pass","Fail","Na") # 3 subvariables per quality metric
@@ -59,9 +57,8 @@ def.qf.finl.l1 <- function (
     }
   }
   
-  nameVarOut <- c(nameVarOut,"qmAlph","qmBeta","qfFinl") # Add alpha QM, beta QM, and final quality flag
-  qfqm<- data.frame(matrix(,ncol=3*numFlag+3))#initial output
-  colnames(qfqm) <- nameVarOut
+  qm<- data.frame(matrix(,ncol=3*numFlag))#initial output
+  colnames(qm) <- nameVarOut
   
   # Compute Quality metric -------------------------------------------------
   # Loop through each flag
@@ -69,27 +66,13 @@ def.qf.finl.l1 <- function (
     
     # Quality metrics for pass, failed, and NA flags
     # Pass
-    qfqm[,(idxFlag-1)*3+1]  <- sum(data[,idxFlag]== 0, na.rm = TRUE)/nrow(data)*100
+    qm[,(idxFlag-1)*3+1]  <- sum(data[,idxFlag]== 0, na.rm = TRUE)/nrow(data)*100
     # Fail
-    qfqm[,(idxFlag-1)*3+2] <- sum(data[,idxFlag]== 1, na.rm = TRUE)/nrow(data)*100
+    qm[,(idxFlag-1)*3+2] <- sum(data[,idxFlag]== 1, na.rm = TRUE)/nrow(data)*100
     # NA
-    qfqm[,(idxFlag-1)*3+3] <- sum(data[,idxFlag]== -1, na.rm = TRUE)/nrow(data)*100 
+    qm[,(idxFlag-1)*3+3] <- sum(data[,idxFlag]== -1, na.rm = TRUE)/nrow(data)*100 
     
   }
-  #compute qfAlpha and qfBeta
-  data[,"qfAlph"] <- apply(data, 1, function(x) ifelse(any(x==1),1,0))
-  #calculate qfBeta
-  data[,"qfBeta"] <- apply(data, 1, function(x) ifelse(any(x==-1),1,0))
   
-  #calculate QMAlpha
-  qfqm[, "qmAlph"] <- sum(data[,"qfAlph"]== 1, na.rm = TRUE)/nrow(data)*100
-  #calculate QMBeta
-  qfqm[, "qmBeta"] <- sum(data[,"qfBeta"]== 1, na.rm = TRUE)/nrow(data)*100  
-  
-  #calculate QF Final
-  if ((rtioAlphBeta[1]*qfqm[, "qmAlph"]) + (rtioAlphBeta[2]*qfqm[, "qmBeta"]) >= thsh) {
-    qfqm[, "qfFinl"] <- 1
-  } else {qfqm[, "qfFinl"] <- 0}
-  
-  return(qfqm)
+  return(qm)
 }
