28ae5e536aca32973b3da50401429d241105b2f7
hongyanluo <hluo@battelleecology.org>
2016-11-23 18:25:04 +0000

    call def.temp.mean.7200() in scripts

--- pack/eddy4R.base/R/wrap.derv.prd.day.R
+++ pack/eddy4R.base/R/wrap.derv.prd.day.R
@@ -77,9 +77,9 @@ wrap.derv.prd.day <- function(
   
 
   # average temperature in irga cell 
-  data$irga$T_cell_7200 <- ff::as.ff(0.2 * data$irga$tempCellIn + 0.8 * data$irga$tempCellOut)
-  base::attr(x = data$irga$T_cell_7200, which = "unit") <- base::attr(x = data$irga$tempCellIn, which = "unit")
-
+  data$irga$T_cell_7200 <- def.temp.mean.7200(tempIn = data$irga$tempCellIn,
+                                              tempOut = data$irga$tempCellOut)
+ 
   # RH in cell
 
     # water vapor partial pressure
