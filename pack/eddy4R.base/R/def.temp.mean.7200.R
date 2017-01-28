0332a4fea71f3720011c628beba05250bddd233c
stefanmet <smetzger@battelleecology.org>
2017-01-28 22:34:24 +0000

    update function header

--- pack/eddy4R.base/R/def.temp.mean.7200.R
+++ pack/eddy4R.base/R/def.temp.mean.7200.R
@@ -1,8 +1,9 @@
 ##############################################################################################
 #' @title Definition function: Calculation of the average temperature in LI-7200 irga cell
 
-#' @author Stefan Metzger \email{eddy4R.info@gmail.com}
-#' Hongyan Luo \email{eddy4R.info@gmail.com}
+#' @author
+#' Stefan Metzger \email{eddy4R.info@gmail.com}
+#' Hongyan Luo \email{hluo@battelleecology.org}
 
 #' @description Function definition. Calculation of the average temperature in LI-7200 IRGA cell
 
@@ -13,7 +14,7 @@
 #' The returned object is the the average temperature in LI-7200 IRGA cell calculated from the temperatrue at IRGA cell inlet and the temperature at IRGA cell outlet.  
 
 #' @references
-#' Currently none.
+#' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007.
 
 #' @keywords temperature, irga
 
