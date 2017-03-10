db5eb05162a0e3856cc8164a735e4d9a1ccfa1fd
hongyanluo <hluo@battelleecology.org>
2017-03-10 22:54:05 +0000

    change 	rereDATA to dataRefe, obsDATA to dataMeas, obsVARS to measVar, maxlag to lagMax, hardlag to lagCnst, nplag to lagNgtvPstv, absolute to lag, FRACmin to fracMin

--- pack/eddy4R.base/R/def.lag.R
+++ pack/eddy4R.base/R/def.lag.R
@@ -10,19 +10,19 @@
 #' @param \code{refe} A vector with variable in reference time frame. Of class numeric. [-]
 #' @param \code{meas} A vector with variable in time frame to be adjusted. Of class numeric. [-]
 #' @param \code{freq_loc} Acquisition frequency of refe and meas. Of class ingeter. [Hz]
-#' @param \code{refeData} A matrix or data.frame with all data that carries the time frame of refe. Defaults to refe. Of any class. [-]
-#' @param \code{measData} A matrix or data.frame with all data that carries the time frame of meas. Defaults to meas. Of any class. [-]
-#' @param \code{measVar} A vector specifying if only several columns in measData shall be lagged. Defaults to NULL. Of class integer or character. [-]
-#' @param \code{maxlag} Maximum lag, by default 2 x freq_loc. Of class integer. [-]
-#' @param \code{hardlag} TRUE - interpret maxlag as maximum permissible lag; FALSE - start with maxlag as first estimate and increase iteratively. Defaults to TRUE. Of class logical. [-]
-#' @param \code{nplag} "n" - consider negative lag times only, i.e. meas is expected to lag behind refe; "p" - consider positive lag times only, i.e. refe is expected to lag behind meas; "np" - consider negative and positive lag times. Defaults to "np". Of class character. [-]
-#' @param \code{absolute} TRUE - consider positive and negative correlations when finding lag time; FALSE - consider positive correlations only when finding lag time. Defaults to TRUE. Of class logical. [-]
+#' @param \code{dataRefe} A matrix or data.frame with all data that carries the time frame of refe. Defaults to refe. Of any class. [-]
+#' @param \code{dataMeas} A matrix or data.frame with all data that carries the time frame of meas. Defaults to meas. Of any class. [-]
+#' @param \code{measVar} A vector specifying if only several columns in dataMeas shall be lagged. Defaults to NULL. Of class integer or character. [-]
+#' @param \code{lagMax} Maximum lag, by default 2 x freq_loc. Of class integer. [-]
+#' @param \code{lagCnst} TRUE - interpret lagMax as maximum permissible lag; FALSE - start with lagMax as first estimate and increase iteratively. Defaults to TRUE. Of class logical. [-]
+#' @param \code{lagNgtvPstv} "n" - consider negative lag times only, i.e. meas is expected to lag behind refe; "p" - consider positive lag times only, i.e. refe is expected to lag behind meas; "np" - consider negative and positive lag times. Defaults to "np". Of class character. [-]
+#' @param \code{lag} TRUE - consider positive and negative correlations when finding lag time; FALSE - consider positive correlations only when finding lag time. Defaults to TRUE. Of class logical. [-]
 #' @param \code{hpf} TRUE - apply Butterworth high-pass filter; FALSE - use raw data. Defaults to TRUE. Of class logical. [-]
-#' @param \code{FRACmin} Minimum fraction of data to attempt lag determination. Defaults to 0.1. Of class numeric. [-]
+#' @param \code{fracMin} Minimum fraction of data to attempt lag determination. Defaults to 0.1. Of class numeric. [-]
 
 #' @return Lagged input data and calculation results in a list consisting of:\cr
-#' \code{refeData} The reference data.
-#' \code{measData} The data that was lagged to coincide with the reference data.
+#' \code{dataRefe} The reference data.
+#' \code{dataMeas} The data that was lagged to coincide with the reference data.
 #' \code{lag} The number of data points by which the lag correction was performed.
 #' \code{ccf} The cross-correction coefficient between refe and meas for the determined lag time.
 
@@ -54,15 +54,15 @@ def.lag <- function(
   refe,
   meas,
   freq_loc,
-  refeData = refe,
-  measData = meas,
+  dataRefe = refe,
+  dataMeas = meas,
   measVar = NULL,
-  maxlag = 2 * freq_loc,
-  hardlag = TRUE,
-  nplag = c("n", "p", "np")[3],
-  absolute = TRUE,
+  lagMax = 2 * freq_loc,
+  lagCnst = TRUE,
+  lagNgtvPstv = c("n", "p", "np")[3],
+  lag = TRUE,
   hpf = TRUE,
-  FRACmin = 0.1
+  fracMin = 0.1
 ) {
   
   ###
@@ -73,8 +73,8 @@ def.lag <- function(
     
     
     output <- list(
-      refeData=refeData,
-      measData=measData,
+      dataRefe=dataRefe,
+      dataMeas=dataMeas,
       lag=NA,
       ccf=NA
     )
@@ -91,12 +91,12 @@ def.lag <- function(
     
     #assign reference and refeervations
     if(is.null(measVar)) {
-      refeData <- as.matrix(refeData)
-      measData <- as.matrix(measData)
+      dataRefe <- as.matrix(dataRefe)
+      dataMeas <- as.matrix(dataMeas)
     } else {
-      refecol <- ncol(refeData)
-      refeData <- cbind(refeData, measData)
-      measData <- as.matrix(measData[,measVar])
+      refecol <- ncol(dataRefe)
+      dataRefe <- cbind(dataRefe, dataMeas)
+      dataMeas <- as.matrix(dataMeas[,measVar])
     }
     
     #fill gaps via linear interpolation
@@ -116,7 +116,7 @@ def.lag <- function(
       #nyquist frequency [Hz]
       NY <- freq_loc / 2
       #cutoff frequency [Hz]
-      cutoff <- 1 / (2 * maxlag / freq_loc)
+      cutoff <- 1 / (2 * lagMax / freq_loc)
       #butterworth filter        
       bf1 <- signal::butter(n=4, W=cutoff/NY, type="high")
       
@@ -142,39 +142,39 @@ def.lag <- function(
     
     
     #find correct lag time    
-    #for hard maxlag argument
-    if(hardlag == TRUE) {
+    #for hard lagMax argument
+    if(lagCnst == TRUE) {
       
       #calculate autocorrelation
-      lagt <- ccf(refe, meas, lag.max = maxlag, plot = FALSE, na.action = na.pass)
+      lagt <- ccf(refe, meas, lag.max = lagMax, plot = FALSE, na.action = na.pass)
       #consider negative lag times only: set correlations for positive lag time to zero
-      if(nplag == "n") lagt$acf[which(lagt$lag > 0)] <- 0          
+      if(lagNgtvPstv == "n") lagt$acf[which(lagt$lag > 0)] <- 0          
       #consider positive lag times only: set correlations for negative lag time to zero
-      if(nplag == "p") lagt$acf[which(lagt$lag < 0)] <- 0
+      if(lagNgtvPstv == "p") lagt$acf[which(lagt$lag < 0)] <- 0
       #determine lag time
-      lag <- ifelse(absolute == FALSE,
+      lag <- ifelse(lag == FALSE,
                     lagt$lag[which(lagt$acf == max(lagt$acf))],		#(-): meas lags behind refe
                     lagt$lag[which(abs(lagt$acf) == max(abs(lagt$acf)))]
       )      
-      #don't lag if determined lag equals maxlag
-      if(abs(lag) == maxlag) lag <- 0
+      #don't lag if determined lag equals lagMax
+      if(abs(lag) == lagMax) lag <- 0
       
-      #for soft maxlag argument
+      #for soft lagMax argument
     } else {
       
-      lag <- maxlag
+      lag <- lagMax
       count <- 1
-      while(abs(lag) == maxlag) {
-        #increase maxlag argument
-        if(count > 1) maxlag <- 2 * maxlag
+      while(abs(lag) == lagMax) {
+        #increase lagMax argument
+        if(count > 1) lagMax <- 2 * lagMax
         #calculate autocorrelation
-        lagt <- ccf(refe, meas, lag.max = maxlag, plot = FALSE, na.action = na.pass)
+        lagt <- ccf(refe, meas, lag.max = lagMax, plot = FALSE, na.action = na.pass)
         #consider negative lag times only: set correlations for positive lag time to zero
-        if(nplag == "n") lagt$acf[which(lagt$lag > 0)] <- 0          
+        if(lagNgtvPstv == "n") lagt$acf[which(lagt$lag > 0)] <- 0          
         #consider positive lag times only: set correlations for negative lag time to zero
-        if(nplag == "p") lagt$acf[which(lagt$lag < 0)] <- 0
+        if(lagNgtvPstv == "p") lagt$acf[which(lagt$lag < 0)] <- 0
         #determine lag time
-        lag <- ifelse(absolute == FALSE,
+        lag <- ifelse(lag == FALSE,
                       lagt$lag[which(lagt$acf == max(lagt$acf))],		#(-): meas lags behind refe
                       lagt$lag[which(abs(lagt$acf) == max(abs(lagt$acf)))]
         )
@@ -183,39 +183,39 @@ def.lag <- function(
       
     }
     
-    #adjust entire measData time series to refeData time (assuming constant timing offset over all variables)
+    #adjust entire dataMeas time series to dataRefe time (assuming constant timing offset over all variables)
     #refe <- lag(refe, k=lag*freq)
     #meas data lags behind refe
     if(lag < 0) {
-      refeData <- refeData[1:(nrow(refeData) + lag),]
-      measData <- measData[(1 - lag):(nrow(measData)),]
+      dataRefe <- dataRefe[1:(nrow(dataRefe) + lag),]
+      dataMeas <- dataMeas[(1 - lag):(nrow(dataMeas)),]
     }
     #refe data lags behind meas
     if(lag > 0) {
       #lag <- lag + 1	#necessary to achieve correct displacement (nested test with ccf)
-      refeData <- refeData[(1 + lag):(nrow(refeData)),]
-      measData <- measData[1:(nrow(measData) - lag),]
+      dataRefe <- dataRefe[(1 + lag):(nrow(dataRefe)),]
+      dataMeas <- dataMeas[1:(nrow(dataMeas) - lag),]
     }
-    #    length(measData)	#50174
+    #    length(dataMeas)	#50174
     
-    #if only certain variables within measData shall be lagged:
+    #if only certain variables within dataMeas shall be lagged:
     if(is.null(measVar)) {
-      refeOUT <- as.matrix(refeData)
-      measOUT <- as.matrix(measData)
+      refeOUT <- as.matrix(dataRefe)
+      measOUT <- as.matrix(dataMeas)
     } else {
-      refeOUT <- as.matrix(refeData[,1:refecol])
-      measOUT <- as.matrix(refeData[,(refecol+1):ncol(refeData)])
-      measOUT[,measVar] <- measData
+      refeOUT <- as.matrix(dataRefe[,1:refecol])
+      measOUT <- as.matrix(dataRefe[,(refecol+1):ncol(dataRefe)])
+      measOUT[,measVar] <- dataMeas
     }
     
     #prepare output
     if(ncol(refeOUT) == 1) refeOUT <- refeOUT[,1]
     if(ncol(measOUT) == 1) measOUT <- measOUT[,1]
     output <- list(
-      refeData=refeOUT,
-      measData=measOUT,
+      dataRefe=refeOUT,
+      dataMeas=measOUT,
       lag=lag,
-      ccf=ifelse(absolute==TRUE, max(abs(lagt$acf)), max(lagt$acf))
+      ccf=ifelse(lag==TRUE, max(abs(lagt$acf)), max(lagt$acf))
     )
     return(output)
     
