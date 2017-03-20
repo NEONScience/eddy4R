faad9f679ed6e8ecf578f04d05d8ea877ad4e8cb
ddurden <daviddurden27@gmail.com>
2017-03-20 19:31:38 +0000

    - Updating the function to grab ParaFlow metadata     - Adding ability to grab ENV variables passed by CI     - Can be defined by function as well

--- pack/eddy4R.base/R/def.para.flow.R
+++ pack/eddy4R.base/R/def.para.flow.R
@@ -34,28 +34,47 @@
 ##############################################################################################################
 
 def.para.flow <- function(
-  ParaFlow = list(
+  DirFilePara  = NULL,
   DirInp = "NA", 
   DirMnt  = "NA",
   DirOut  = "NA",
   DirTmp  = "NA",
   DirWrk  = "NA",
-  Loc  = NULL,
-  DirFilePara  = NULL,
   FileDp0p  = NULL,
+  Loc  = NULL,
   Read  = "hdf5",
   VersDp  = c("001","004")[1],
-  VersEddy  = "cybi"),
+  VersEddy  = "cybi",
+  MethParaFlow = c("DfltInp","EnvVar")[1],
   ...
 ){
   
-  #Grab the 
- # ParaFlow <- as.list(match.call(expand.dots = T))[-1]
-
-  print(ParaFlow$DirInp)
- # Para
+  ParaFlow <- list(DirFilePara = DirFilePara,DirInp = DirInp,DirMnt = DirMnt,DirOut = DirOut,DirTmp = DirTmp,DirWrk = DirWrk,FileDp0p = FileDp0p,Loc = Loc,MethParaFlow = MethParaFlow,Read = Read,VersDp = VersDp,VersEddy = VersEddy, ...)
   
-  #ParaFlow <- list(ls())
-  return(ParaFlow)
+  if(MethParaFlow == "EnvVar"){
+    #Create a list with all the specified function arguments
+    
+    
+    if("DIRFILEPARA" %in% base::names(base::Sys.getenv())) {ParaFlow$DirFilePara <- Sys.getenv("DIRFILEPARA")} else {warning("The variable ParaFlow$DirFilePara is not specified as ENV variable")}
+    if("DIRINP" %in% base::names(base::Sys.getenv())) {ParaFlow$DirInp <- Sys.getenv("DIRINP")} else {warning("The variable ParaFlow$DirInp is not specified as ENV variable")}
+    if("DIRMNT" %in% base::names(base::Sys.getenv())) {ParaFlow$DirMnt <- Sys.getenv("DIRMNT")} else {warning("The variable ParaFlow$DirMnt is not specified as ENV variable")}
+    if("DIROUT" %in% base::names(base::Sys.getenv())) {ParaFlow$DirOut <- Sys.getenv("DIROUT")} else {warning("The variable ParaFlow$DirOut is not specified as ENV variable")}
+    if("DIRTMP" %in% base::names(base::Sys.getenv())) {ParaFlow$DirTmp <- Sys.getenv("DIRTMP")} else {warning("The variable ParaFlow$DirTmp is not specified as ENV variable")}
+    if("DIRWRK" %in% base::names(base::Sys.getenv())) {ParaFlow$DirWrk <- Sys.getenv("DIRWRK")} else {warning("The variable ParaFlow$DirWrk is not specified as ENV variable")}
+    if("FILEDP0P" %in% base::names(base::Sys.getenv())) {ParaFlow$FileDp0p <- Sys.getenv("FILEDP0P")} else {warning("The variable ParaFlow$FileDp0p is not specified as ENV variable")}
+    if("LOC" %in% base::names(base::Sys.getenv())) {ParaFlow$Loc <- Sys.getenv("LOC")} else {warning("The variable ParaFlow$Loc is not specified as ENV variable")}
+    if("METHPARAFLOW" %in% base::names(base::Sys.getenv())) {ParaFlow$MethParaFlow <- Sys.getenv("METHPARAFLOW")} else {warning("The variable ParaFlow$MethParaFlow is not specified as ENV variable")}
+    if("READ" %in% base::names(base::Sys.getenv())) {ParaFlow$Read <- Sys.getenv("READ")} else {warning("The variable ParaFlow$Read is not specified as ENV variable")}
+    if("VERSDP" %in% base::names(base::Sys.getenv())) {ParaFlow$VersDp <- Sys.getenv("VERSDP")} else {warning("The variable ParaFlow$VersDp is not specified as ENV variable")}
+    if("VERSEDDY" %in% base::names(base::Sys.getenv())) {ParaFlow$VersEddy <- Sys.getenv("VERSEDDY")} else {warning("The variable ParaFlow$VersEddy is not specified as ENV variable")}
+    
+  }
+  
+  
+    if(is.null(ParaFlow$FileDp0p)|is.null(ParaFlow$DirFilePara)){stop("DirFilePara and FileDp0p must be defined.")}
+    if(is.null(ParaFlow$Loc)) warning("The variable Loc is NULL") 
+  #Grab the 
+
+ return(ParaFlow)
   
 }
