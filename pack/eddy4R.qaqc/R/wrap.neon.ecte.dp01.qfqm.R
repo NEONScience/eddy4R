949583fbff062a2d57e3e2a3a038489e54944719
NDurden <ndurden@neoninc.org>
2017-03-23 20:42:48 +0000

    revised wrap.neon.ecte.dp01.qfqm by adding functionality to include qf from ECSE

--- pack/eddy4R.qaqc/R/wrap.neon.ecte.dp01.qfqm.R
+++ pack/eddy4R.qaqc/R/wrap.neon.ecte.dp01.qfqm.R
@@ -1,15 +1,16 @@
 ##############################################################################################
-#' @title Wrapper function: Calculate quality metrics, alpha and beta quality metrics, and final quality flag for the NEON eddy-covariance turbulent exchange data products
+#' @title Wrapper function: Calculate quality metrics, alpha and beta quality metrics, and final quality flag for the NEON eddy-covariance turbulent and stroage exchange L1 data products
 
 #' @author
 #' Natchaya Pingintha-Durden \email{ndurden@battelleecology.org}
 
-#' @description Wrapper function. Calculate quality metrics, alpha and beta quality metrics, and final quality flag for the NEON eddy-covariance turbulent exchange data products.
+#' @description Wrapper function. Calculate quality metrics, alpha and beta quality metrics, and final quality flag for the NEON eddy-covariance turbulent and storage exchange L1 data products.
 
-#' @param \code{qfSens00} A data frame of quality flags, class integer, which related to NEON eddy-covariance turbulent exchange L1 data products that the quality metrics and final quality flags are being calculated. Each column contains the quality flag values [-1,0,1] for that flag. [-] 
-#' @param \code{qfSens01} Optional. A data frame of quality flags, class integer, which related to NEON eddy-covariance turbulent exchange L1 data products that the quality metrics and final quality flags are being calculated. Each column contains the quality flag values [-1,0,1] for that flag. [-] 
-#' @param \code{qfSens02} Optional. A data frame of quality flags, class integer, which related to NEON eddy-covariance turbulent exchange L1 data products that the quality metrics and final quality flags are being calculated. Each column contains the quality flag values [-1,0,1] for that flag. [-] 
-#' @param \code{dp01} A vector of class "character" containing the name of NEON eddy-covariance turbulent exchange L1 data products which the quality metrics and final quality flags are being calculated, c("irgaCo2","irgaH2o","soni","soniAmrs"). Defaults to "irgaCo2". [-]
+#' @param \code{qfInput} A list of data frame containing the input quality flag data that related to L1 data products are being grouped. Of class integer". [-] 
+#' @param \code{MethMeas} A vector of class "character" containing the name of measurement method (eddy-covariance turbulent exchange or storage exchange), MethMeas = c("ecte", "ecse"). Defaults to "ecse". [-] 
+#' @param \code{TypeMeas} A vector of class "character" containing the name of measurement type (sampling or validation), TypeMeas = c("samp", "ecse"). Defaults to "samp". [-]
+#' @param \code{dp01} A vector of class "character" containing the name of NEON ECTE and ECSE L1 data products which the flags are being grouped, \cr
+#' c("envHut", "irgaCo2", "irgaH2o", "isoCo2", "isoH2o", "soni", "soniAmrs", "tempAirLvl", "tempAirTop"). Defaults to "irgaCo2". [-] 
 
 #' @return A list of: \cr
 #' \code{qm}  A list of data frame containing quality metrics (fractions) of failed, pass, and NA for each of the individual flag which related to L1 sub-data products. [fraction] \cr
@@ -27,17 +28,17 @@
 #' #generate the fake quality flags for each sensor
 #' TimeBgn <- "2016-04-24 02:00:00.000"
 #' TimeEnd <- "2016-04-24 02:29:59.950"
-#' inp <- list()
-#' inp$qf$irga <- eddy4R.qaqc::def.qf.ecte(TimeBgn = TimeBgn, TimeEnd = TimeEnd, Freq = 20, Sens = "irga", PcntQf = 0.05)
-#' inp$qf$irgaMfcSamp <- eddy4R.qaqc::def.qf.ecte(TimeBgn = TimeBgn, TimeEnd = TimeEnd, Freq = 20, Sens = "irgaMfcSamp", PcntQf = 0.05)
-#' inp$qf$soni <- eddy4R.qaqc::def.qf.ecte(TimeBgn = TimeBgn, TimeEnd = TimeEnd, Freq = 20, Sens = "soni", PcntQf = 0.05)
-#' inp$qf$soniAmrs <- eddy4R.qaqc::def.qf.ecte(TimeBgn = TimeBgn, TimeEnd = TimeEnd, Freq = 40, Sens = "soniAmrs", PcntQf = 0.05)
+#' qf <- list()
+#' qf$irga <- eddy4R.qaqc::def.qf.ecte(TimeBgn = TimeBgn, TimeEnd = TimeEnd, Freq = 20, Sens = "irga", PcntQf = 0.05)
+#' qf$irgaMfcSamp <- eddy4R.qaqc::def.qf.ecte(TimeBgn = TimeBgn, TimeEnd = TimeEnd, Freq = 20, Sens = "irgaMfcSamp", PcntQf = 0.05)
+#' qf$soni <- eddy4R.qaqc::def.qf.ecte(TimeBgn = TimeBgn, TimeEnd = TimeEnd, Freq = 20, Sens = "soni", PcntQf = 0.05)
+#' qf$soniAmrs <- eddy4R.qaqc::def.qf.ecte(TimeBgn = TimeBgn, TimeEnd = TimeEnd, Freq = 40, Sens = "soniAmrs", PcntQf = 0.05)
 #' #calculate quality metric, qmAlpha, qmBeta, qfFinl
 #' qfqm <- list()
-#' qfqm$irgaCo2 <- wrap.neon.ecte.dp01.qfqm (qfSens00 = inp$qf$irga, qfSens01 = inp$qf$irgaMfcSamp, dp01 = "irgaCo2")
-#' qfqm$irgaH2o <- wrap.neon.ecte.dp01.qfqm (qfSens00 = inp$qf$irga, qfSens01 = inp$qf$irgaMfcSamp, dp01 = "irgaH2o")
-#' qfqm$soni <- wrap.neon.ecte.dp01.qfqm (qfSens00 = inp$qf$soni, qfSens01 = inp$qf$irga, dp01 = "soni")
-#' qfqm$soniAmrs <- wrap.neon.ecte.dp01.qfqm (qfSens00 = inp$qf$soniAmrs, dp01 = "soniAmrs")
+#' qfqm$irgaCo2 <- wrap.neon.dp01.qfqm (qfInput = qf, MethMeas = "ecte", TypeMeas = "samp", dp01="irgaCo2")
+#' qfqm$irgaH2o <- wrap.neon.dp01.qfqm (qfInput = qf, MethMeas = "ecte", TypeMeas = "samp", dp01="irgaH2o")
+#' qfqm$soni <- wrap.neon.dp01.qfqm (qfInput = qf, MethMeas = "ecte", TypeMeas = "samp", dp01="soni")
+#' qfqm$soniAmrs <- wrap.neon.dp01.qfqm (qfInput = qf, MethMeas = "ecte", TypeMeas = "samp", dp01="soniAmrs")
 
 #' @seealso Currently none
 
@@ -48,20 +49,23 @@
 #     original creation
 #   Natchaya P-Durden (2016-12-09)
 #     replaced for loop by lapply
+#   Natchaya P-Durden (2017-03-23)
+#     revised original function to wrap.neon.dp01.qfqm ()
+#     added ECSE quality flags
 ##############################################################################################
 
-wrap.neon.ecte.dp01.qfqm <- function(
-  qfSens00,
-  qfSens01 = NULL,
-  qfSens02 = NULL,
-  dp01 = c("irgaCo2","irgaH2o","soni","soniAmrs")[1]
+wrap.neon.dp01.qfqm <- function(
+  qfInput = list(),
+  MethMeas = c("ecte", "ecse")[1],
+  TypeMeas = c("samp", "vali")[1], 
+  dp01 = c("envHut", "irgaCo2", "irgaH2o", "isopCo2", "isopH2o", "soni", "soniAmrs", "tempAirLvl", "tempAirTop")[1]
 ) {
 
   #assign list
   rpt <- list()
   tmp <- list()
   #grouping qf
-  inp <- def.neon.ecte.dp01.qf.grup(qfSens00 = qfSens00, qfSens01 = qfSens01, qfSens02 = qfSens02, dp01=dp01)
+  inp <- def.neon.dp01.qf.grp(qfInput = qfInput, MethMeas = MethMeas, TypeMeas = TypeMeas, dp01=dp01)
   
   #calculate qmAlpha, qmBeta, qfFinl
   tmp <- lapply(inp, FUN = eddy4R.qaqc::def.qf.finl)
