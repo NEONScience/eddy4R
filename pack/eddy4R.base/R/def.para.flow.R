fb7fa0d4079cb2a4af4f46f7a4e4ca1d6ea46329
Ke Xu <kxu@battelleecology.org>
2017-05-02 03:21:41 +0000

    update dropbox input link

--- pack/eddy4R.base/R/def.para.flow.R
+++ pack/eddy4R.base/R/def.para.flow.R
@@ -90,7 +90,7 @@ def.para.flow <- function(
       # input data
       
       # download data
-      eddy4R.base::def.dld.zip(Inp = list(Url = "https://www.dropbox.com/s/q1add2a3vg5g228/inpRefe_2.zip?dl=1",
+      eddy4R.base::def.dld.zip(Inp = list(Url = "https://www.dropbox.com/s/xb6ol3rxnwi0k3r/inpRefe.zip?dl=1",
                                           Dir = tempdir()))
       
       # assign corresponding DirFilePara
