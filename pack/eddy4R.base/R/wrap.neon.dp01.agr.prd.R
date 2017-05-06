37e19a580c44bbd22895b4682ce25426ef83b2f9
ddurden <daviddurden27@gmail.com>
2017-05-06 21:20:56 +0000

    - Adding documentation

--- pack/eddy4R.base/R/wrap.neon.dp01.agr.prd.R
+++ pack/eddy4R.base/R/wrap.neon.dp01.agr.prd.R
@@ -1,3 +1,49 @@
+##############################################################################################
+#' @title Wrapper function: Create NEON Level 1 data products with quality flags and quality metrics for different aggregation periods 
+
+#' @author
+#' David Durden \email{ddurden@battelleecology.org}
+
+#' @description 
+#' Wrapper function. Compute NEON Level 1 data products with quality flags and quality metrics for different aggregation periods (e.g. 1 minute).
+
+#' @param inpList A list of including dp0p data and quality flags to perform dp01 calculation and quality tests.
+
+#' @return A list containing dp01 data and quality flags and metrics for all sensors including the following: \cr
+#' \code{data}:
+  #' \code{mean} The mean of non-NA values in \code{data}
+  #' \code{min} The minimum value of non-NA values in \code{data}
+  #' \code{max} The maximum value of non-NA values in \code{data}
+  #' \code{vari} The variance of non-NA values in \code{data}
+  #' \code{num} The number of non-NA values in \code{data}
+  #' \code{se} The standard error of the mean of non-NA values in \code{data}
+#'\code{qfqm}: 
+  #' \code{qm}  A list of data frame's containing quality metrics (fractions) of failed, pass, and NA for each of the individual flag which related to L1 sub-data products if RptExpd = TRUE. [fraction] \cr
+  #' \code{qmAlph} A dataframe containing metrics in a  columns of class "numeric" containing the alpha quality metric for L1 sub-data products. [fraction] \cr
+  #' \code{qmBeta} A dataframe containing metrics in a columns of class "numeric" containing the beta quality metric for L1 sub-data products. [fraction] \cr
+  #' \code{qfFinl} A dataframe containing flags in a columns of class "numeric", [0,1], containing the final quality flag for L1 sub-data products. [-] \cr
+  #' \code{qfSciRevw} A dataframe containing flags in a columns of class "numeric", [0,1], containing the scientific review quality flag for L1 sub-data products. [-] \cr
+
+#' @references
+#' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007.
+
+#' @keywords average, aggregate, descriptive statistics, NEON QAQC, quality flags, quality metrics
+
+#' @examples 
+
+
+#' @references
+#' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007.
+
+
+#' @seealso Currently none.
+
+#' @export
+
+# changelog and author contributions / copyrights 
+#   David Durden (2017-05-05)
+#     original creation
+##############################################################################################
 
 
 wrap.neon.dp01.agr.prd <- function(
