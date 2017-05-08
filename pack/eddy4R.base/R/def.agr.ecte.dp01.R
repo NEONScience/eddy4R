9bee372453f050fe5099b5688770d75468ed917f
ddurden <daviddurden27@gmail.com>
2017-05-08 09:13:30 +0000

    - Adding method to deal with subaggregated data

--- pack/eddy4R.base/R/def.agr.ecte.dp01.R
+++ pack/eddy4R.base/R/def.agr.ecte.dp01.R
@@ -8,7 +8,8 @@
 #' Definition function to produce a dataframe of indices and corresponding times for aggregation periods.
 #'
 #' @param inpList a list of dp01 computed output statistics and dp01 quality flags and quality metrics over multiple aggregations periods that need to be combined.
-#' 
+#'@param MethSubAgr a logical to indicate if the subset aggregated periods should be included in the data to be combined.
+#'  
 #' @return A dataframe of indices and corresponding times for aggregation periods.
 
 #' @references 
@@ -29,22 +30,30 @@
 ##############################################################################################
 
 def.agr.ecte.dp01 <- function(
-inpList 
-  
+inpList, 
+MethSubAgr = FALSE  
 ){
   
   # concatenate results
   rpt <- list()
   # timestamps
-
+  names(inpList$idx)[which(names(inpList$idx) == "irga")] <- "irgaCo2"
+  names(inpList$idx)[which(names(inpList$idx) == "irgaMfcSamp")] <- "irgaH2o"
+  
+  #Use irga as the standard time for outputs
+  rpt$time <- data.frame(
+    timeBgn = inpList$idx$irgaH2o$timeBgn,
+    timeEnd = inpList$idx$irgaH2o$timeEnd
+  )[1:length(inpList$dp01),]
   
   # loop around data products
   for(idxDp01 in names(inpList$dp01[[1]])) {
     
-    rpt$time <- data.frame(
+   
+     rpt$time[[idxDp01]] <- data.frame(
       timeBgn = inpList$idx[[idxDp01]]$timeBgn,
       timeEnd = inpList$idx[[idxDp01]]$timeEnd
-    )
+    )[1:length(inpList$dp01),]
     
     # combine data for different time periods
     # http://stackoverflow.com/questions/10832288/coerce-a-specific-sublist-of-a-list-to-a-matrix-data-frame-with-base-r
@@ -79,8 +88,49 @@ inpList
     # assign names to data.frames      
     names(rpt$qfqm[[idxDp01]]) <- names(inpList$qfqmOut[[1]][[idxDp01]])
     
+    if(MethSubAgr == TRUE){
+
+########################################################################################
+      #Data
+    rpt$dp01AgrSub$data[[idxDp01]] <- 
+      
+      # first call to lapply, targeting the result data.frames to be created (data sub-products: mean, min, max, vari", numSamp)
+      lapply(names(inpList$dp01AgrSub[[1]]$data[[idxDp01]]), function(y) 
+        
+        # second call to lapply, targeting the observations to be combined into the result data.frames
+        do.call(rbind, lapply(1:length(inpList$dp01AgrSub), function(x) inpList$dp01AgrSub[[x]]$data[[idxDp01]][[y]] ))
+        
+      )
     
-  }
+    names(rpt$dp01AgrSub$data[[idxDp01]]) <- names(inpList$dp01AgrSub[[1]]$data[[idxDp01]])
+###################################################################################
+    # QFQM
+    rpt$dp01AgrSub$qfqm[[idxDp01]] <- 
+      
+      # first call to lapply, targeting the result data.frames to be created (data sub-products: mean, min, max, vari", numSamp)
+      lapply(names(inpList$dp01AgrSub[[1]]$qfqm[[idxDp01]]), function(y) 
+        
+        # second call to lapply, targeting the observations to be combined into the result data.frames
+        do.call(rbind, lapply(1:length(inpList$dp01AgrSub), function(x) inpList$dp01AgrSub[[x]]$qfqm[[idxDp01]][[y]] ))
+      )
+    
+    names(rpt$dp01AgrSub$qfqm[[idxDp01]]) <- names(inpList$dp01AgrSub[[1]]$qfqm[[idxDp01]])
+
+###################################################################################
+    # Time
+    rpt$dp01AgrSub$time[[idxDp01]] <- 
+      
+      # first call to lapply, targeting the result data.frames to be created (data sub-products: mean, min, max, vari", numSamp)
+      lapply(length(inpList$dp01AgrSub), function(y) 
+        
+        # second call to lapply, targeting the observations to be combined into the result data.frames
+        do.call(rbind, inpList$dp01AgrSub[[y]]$time))
+    
+   # names(rpt$dp01AgrSub$time) <- names(inpList$dp01AgrSub[[1]]$time)
+    
+    }
+    
+    }
   
   
   #Return output
