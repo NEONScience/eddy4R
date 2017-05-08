553f43ddfea0a05f0d5edea9540bb09b0abbc1e8
ddurden <daviddurden27@gmail.com>
2017-05-08 09:12:32 +0000

    - Adding a function to package data to be written to a HDF5 file

---
+++ pack/eddy4R.base/R/def.hdf5.dp01.pack.R
@@ -0,0 +1,66 @@
+##############################################################################################
+#' @title Definition function: to package dp01 dp01 outputs to be written to HDF5 files
+
+#' @author 
+#' Dave Durden \email{ddurden@battelleecology.org} \cr
+
+#' @description 
+#' Definition function to produce a list of dataframes corresponding times for aggregation periods for dp01, and are packaged to be written to the HDF5 file.
+#'
+#' @param inpList a list of dp01 computed output statistics or dp01 quality flags and quality metrics over multiple aggregations periods that need to be combined and formatted for output to HDF5.
+#'@param time a dataframe including the timeBgn and timeEnd for the aggregated periods should be included in the data to be combined.
+#'  
+#' @return A list of dataframes of for aggregation periods.
+
+#' @references 
+#' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007.
+
+#' @keywords NEON, dp01, averaging intervals, HDF5
+
+#' @examples 
+
+
+#' @seealso Currently none
+
+#' @export
+
+# changelog and author contributions / copyrights
+#   Dave Durden (2017-05-07)
+#     original creation
+##############################################################################################
+
+
+
+def.hdf5.dp01.pack <- function(
+  inpList,
+  time
+){
+
+rpt <- list()
+tmp <- list()
+
+for(idxVar in names(inpList[[idxDp01]][[1]])) {
+  print(idxVar)  
+  lapply(names(inpList[[idxDp01]]), function(x) {
+    tmp[[idxVar]][[x]] <<- inpList[[idxDp01]][[x]][,idxVar]
+  }) 
+  
+};rm(idxVar)
+
+
+rpt <- lapply(names(tmp), function(x) data.frame(do.call("cbind", tmp[[x]])))
+
+names(rpt) <- names(tmp)
+
+rpt <- lapply(rpt, cbind, timeBgn = strftime(time$timeBgn, format="%Y-%m-%dT%H:%M:%OSZ", tz="UTC"), timeEnd = strftime(time$timeEnd, format="%Y-%m-%dT%H:%M:%OSZ", tz="UTC"), stringsAsFactors = FALSE)
+
+
+for(idxVar in base::names(inpList[[idxDp01]][[1]])) {
+  
+  base::attr(x = rpt[[idxVar]], which = "unit") <-
+    base::attr(x = inpList[[idxDp01]][[1]][,idxVar], which = "unit")
+  
+}; rm(idxVar)
+
+return(rpt)
+}
