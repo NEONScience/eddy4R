f3508705e45c4fa99d4d1a752706f505f243db2e
ddurden <daviddurden27@gmail.com>
2017-06-30 09:17:29 +0000

    - Adding switch for 1-minute output

--- pack/eddy4R.base/R/def.hdf5.wrte.dp01.R
+++ pack/eddy4R.base/R/def.hdf5.wrte.dp01.R
@@ -12,6 +12,7 @@
 #' @param SiteLoca Character: Site location.
 #' @param LevlTowr Character: The tower level that the sensor data is being collected in NEON data product convention (HOR_VER).
 #' @param MethUcrt Logical: Determines if uncertainty information is available for output.
+#' @param MethSubAgr Logical: Determines if 1-minute data is available for output.
 #' 
 #' @return An HDF5 file with dp01 data, qfqm, and uncertainty written
 #' 
@@ -43,7 +44,8 @@ def.hdf5.wrte.dp01 <- function(
   SiteLoca,
   LevlTowr,
   Dp01,
-  MethUcrt = TRUE
+  MethUcrt = TRUE,
+  MethSubAgr = TRUE
 ){
   
 #Determine if the output file should be expanded or basic by creating a logical determined from the filename
@@ -78,9 +80,6 @@ idUcrt30 <- rhdf5::H5Gopen(fid,paste0("/", SiteLoca, "/dp01/ucrt/",Dp01,"/",Levl
 #Writing 30-min data to output HDF5 file
 lapply(names(inpList$data[[Dp01]]), function(x) rhdf5::h5writeDataset.data.frame(obj = inpList$data[[Dp01]][[x]], h5loc = idData30, name = x, DataFrameAsCompound = TRUE))
 
-#Writing sub-aggregated (e.g.1-min) data to output HDF5 file
-lapply(names(inpList$dp01AgrSub$data[[Dp01]]), function(x) rhdf5::h5writeDataset.data.frame(obj = inpList$dp01AgrSub$data[[Dp01]][[x]], h5loc = idData01, name = x, DataFrameAsCompound = TRUE))
-
 #Writing 30-min data unit attributes to output HDF5 file
 lapply(names(inpList$data[[Dp01]]), function(x) {
   if (!is.null(attributes(inpList$data[[Dp01]][[x]])$unit) == TRUE){
@@ -88,13 +87,17 @@ lapply(names(inpList$data[[Dp01]]), function(x) {
     rhdf5::h5writeAttribute(attributes(inpList$data[[Dp01]][[x]])$unit, h5obj = dgid, name = "unit")
   }})
 
+if(MethSubAgr == TRUE){
+#Writing sub-aggregated (e.g.1-min) data to output HDF5 file
+lapply(names(inpList$dp01AgrSub$data[[Dp01]]), function(x) rhdf5::h5writeDataset.data.frame(obj = inpList$dp01AgrSub$data[[Dp01]][[x]], h5loc = idData01, name = x, DataFrameAsCompound = TRUE))
+
 #Writing sub-aggregated (e.g.1-min) data unit attributes to output HDF5 file
 lapply(names(inpList$dp01AgrSub$data[[Dp01]]), function(x) {
   if (!is.null(attributes(inpList$dp01AgrSub$data[[Dp01]][[x]])$unit) == TRUE){
     dgid <- rhdf5::H5Dopen(idData01, x)
     rhdf5::h5writeAttribute(attributes(inpList$dp01AgrSub$data[[Dp01]][[x]])$unit, h5obj = dgid, name = "unit")
   }})
-
+}
 ##########################################################################################
 #QFQM
 ##########################################################################################
@@ -109,6 +112,7 @@ lapply(names(inpList$qfqm[[Dp01]]), function(x)  {
   #Write 30-min qfqm output to HDF5
   rhdf5::h5writeDataset.data.frame(obj = inpList$qfqm[[Dp01]][[x]][,c("qfFinl","timeBgn","timeEnd")], h5loc = idQfqm30, name = x, DataFrameAsCompound = TRUE)})
 
+  if(MethSubAgr == TRUE){
 #Writing sub-aggregated (e.g.1-min) qfqm to output HDF5 file
 lapply(names(inpList$dp01AgrSub$qfqm[[Dp01]]), function(x)  {
   #convert to integer
@@ -117,7 +121,8 @@ lapply(names(inpList$dp01AgrSub$qfqm[[Dp01]]), function(x)  {
   inpList$dp01AgrSub$qfqm[[Dp01]][[x]]$qfSciRevw <<- as.integer(inpList$dp01AgrSub$qfqm[[Dp01]][[x]]$qfSciRevw) 
   #Write 1-min output to HDF5
   rhdf5::h5writeDataset.data.frame(obj = inpList$dp01AgrSub$qfqm[[Dp01]][[x]][,c("qfFinl","timeBgn","timeEnd")], h5loc = idQfqm01, name = x, DataFrameAsCompound = TRUE)})
-
+  }
+    
 } else {
   
   lapply(names(inpList$qfqm[[Dp01]]), function(x)  {
@@ -128,6 +133,15 @@ lapply(names(inpList$dp01AgrSub$qfqm[[Dp01]]), function(x)  {
     #Write 30-min qfqm output to HDF5
     rhdf5::h5writeDataset.data.frame(obj = inpList$qfqm[[Dp01]][[x]], h5loc = idQfqm30, name = x, DataFrameAsCompound = TRUE)})
   
+  #Writing 30-min qfqm unit attributes to output HDF5 file
+  lapply(names(inpList$qfqm[[Dp01]]), function(x) {
+    if (!is.null(attributes(inpList$qfqm[[Dp01]][[x]])$unit) == TRUE){
+      dgid <- rhdf5::H5Dopen(idQfqm30, x)
+      rhdf5::h5writeAttribute(attributes(inpList$qfqm[[Dp01]][[x]])$unit, h5obj = dgid, name = "unit")
+    }})
+
+#Write 1-min qfqm
+if(MethSubAgr == TRUE){  
   #Writing sub-aggregated (e.g.1-min) qfqm to output HDF5 file
   lapply(names(inpList$dp01AgrSub$qfqm[[Dp01]]), function(x)  {
     #convert to integer
@@ -137,22 +151,16 @@ lapply(names(inpList$dp01AgrSub$qfqm[[Dp01]]), function(x)  {
     #Write 1-min output to HDF5
     rhdf5::h5writeDataset.data.frame(obj = inpList$dp01AgrSub$qfqm[[Dp01]][[x]], h5loc = idQfqm01, name = x, DataFrameAsCompound = TRUE)})
   }
+}
 
-
-#Writing 30-min qfqm unit attributes to output HDF5 file
-lapply(names(inpList$qfqm[[Dp01]]), function(x) {
-  if (!is.null(attributes(inpList$qfqm[[Dp01]][[x]])$unit) == TRUE){
-    dgid <- rhdf5::H5Dopen(idQfqm30, x)
-    rhdf5::h5writeAttribute(attributes(inpList$qfqm[[Dp01]][[x]])$unit, h5obj = dgid, name = "unit")
-  }})
-
+if(MethSubAgr == TRUE){
 #Writing sub-aggregated (e.g.1-min) qfqm unit attributes to output HDF5 file
 lapply(names(inpList$dp01AgrSub$qfqm[[Dp01]]), function(x) {
   if (!is.null(attributes(inpList$dp01AgrSub$qfqm[[Dp01]][[x]])$unit) == TRUE){
     dgid <- rhdf5::H5Dopen(idQfqm01, x)
     rhdf5::h5writeAttribute(attributes(inpList$dp01AgrSub$qfqm[[Dp01]][[x]])$unit, h5obj = dgid, name = "unit")
   }})
-
+}
 ##########################################################################################
 #Ucrt
 ##########################################################################################
@@ -160,16 +168,17 @@ if(MethUcrt == TRUE){
 
 #Writing 30-min ucrt to output HDF5 file
 lapply(names(inpList$ucrt[[Dp01]]), function(x) rhdf5::h5writeDataset.data.frame(obj = inpList$ucrt[[Dp01]][[x]], h5loc = idUcrt30, name = x, DataFrameAsCompound = TRUE))
-
-#Writing sub-aggregated (e.g.1-min) ucrt to output HDF5 file
-lapply(names(inpList$dp01AgrSub$ucrt[[Dp01]]), function(x) rhdf5::h5writeDataset.data.frame(obj = inpList$dp01AgrSub$ucrt[[Dp01]][[x]], h5loc = idUcrt01, name = x, DataFrameAsCompound = TRUE))
-
+  
 #Writing 30-min ucrt unit attributes to output HDF5 file
 lapply(names(inpList$ucrt[[Dp01]]), function(x) {
   if (!is.null(attributes(inpList$ucrt[[Dp01]][[x]])$unit) == TRUE){
     dgid <- rhdf5::H5Dopen(idUcrt30, x)
     rhdf5::h5writeAttribute(attributes(inpList$ucrt[[Dp01]][[x]])$unit, h5obj = dgid, name = "unit")
   }})
+  
+if(MethSubAgr == TRUE){
+#Writing sub-aggregated (e.g.1-min) ucrt to output HDF5 file
+lapply(names(inpList$dp01AgrSub$ucrt[[Dp01]]), function(x) rhdf5::h5writeDataset.data.frame(obj = inpList$dp01AgrSub$ucrt[[Dp01]][[x]], h5loc = idUcrt01, name = x, DataFrameAsCompound = TRUE))
 
 #Writing sub-aggregated (e.g.1-min) ucrt unit attributes to output HDF5 file
 lapply(names(inpList$dp01AgrSub$ucrt[[Dp01]]), function(x) {
@@ -178,7 +187,7 @@ lapply(names(inpList$dp01AgrSub$ucrt[[Dp01]]), function(x) {
     rhdf5::h5writeAttribute(attributes(inpList$dp01AgrSub$ucrt[[Dp01]][[x]])$unit, h5obj = dgid, name = "unit")
   }})
 }
-
+}
 #Close HDF5 connections
 rhdf5::H5close()
 }
