2c2094f07d3f6fc62936bc0405645a79d353f1c7
ddurden <daviddurden27@gmail.com>
2017-09-14 16:20:23 +0000

    - Adding documentation

--- pack/eddy4R.base/R/def.hdf5.crte.R
+++ pack/eddy4R.base/R/def.hdf5.crte.R
@@ -11,8 +11,9 @@
 #' @param Site is the site for which the output file is being generated.
 #' @param LevlTowr is the measurement level of the tower top to determine the VER number of the NEON DP naming convention.
 #' @param DirOut is the output directory where the file being generated is stored.
-#' @param Dom is the NEON domain
-#' @param MethExpd logical indicating if the output should be expanded or basic
+#' @param Dom is the NEON domain.
+#' @param MethExpd logical indicating if the output should be expanded or basic.
+#' @param MethDp04 logical indicating if ECTE dp04 HDF5 folder structure should be included.
 #' @param fileNameReadMe character indicating the filename incl. absolute path to the ReadMe file for inclusion in the output HDF5 file. Defaults to \code{NULL}, which downloads the readme file from a web location.
 #' @param fileNameObjDesc character indicating the filename incl. absolute path to the object description file for inclusion in the output HDF5 file. Defaults to \code{NULL}, which downloads the object description file from a web location.
 
@@ -57,6 +58,8 @@
 #     Adding uncertainty level output
 #   Stefan Metzger (2017-07-01)
 #     added switch for readme and object description file
+#   Dave Durden (2017-09-07)
+#     Adding dp04 method switch
 
 ##############################################################################################################
 #Start of function call to generate NEON HDF5 files
@@ -69,7 +72,7 @@ def.hdf5.crte <- function(
   DirOut, 
   Dom = Dom,
   MethExpd = TRUE,
-  MethDp04 = TRUE,
+  MethDp04 = FALSE,
   fileNameReadMe = NULL,
   fileNameObjDesc = NULL
   ) {
@@ -142,12 +145,12 @@ def.hdf5.crte <- function(
   idDp0p <- rhdf5::H5Gcreate(idSite,"dp0p")
   idDp01 <- rhdf5::H5Gcreate(idSite,"dp01")
 
-  
+  #Adding structure under dp0p
   idDataLvlDp0p <- rhdf5::H5Gcreate(idDp0p,"data")
-  idDataLvlDp01 <- rhdf5::H5Gcreate(idDp01,"data")
-
-  
   idQfqmLvlDp0p <- rhdf5::H5Gcreate(idDp0p,"qfqm")
+  
+  #Adding structure under dp01
+  idDataLvlDp01 <- rhdf5::H5Gcreate(idDp01,"data")
   idQfqmLvlDp01 <- rhdf5::H5Gcreate(idDp01,"qfqm")
   idUcrtLvlDp01 <- rhdf5::H5Gcreate(idDp01,"ucrt")
 
