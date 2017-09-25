69bad83bdd51706e89a8062c893b11112b64c855
ddurden <daviddurden27@gmail.com>
2017-09-25 17:30:04 +0000

    - Updating the definition function to also grab Loc parameter

--- pack/eddy4R.base/R/def.para.levl.towr.R
+++ pack/eddy4R.base/R/def.para.levl.towr.R
@@ -1,15 +1,15 @@
 ##############################################################################################
-#' @title Definition function: Determine tower top measurement level from input dp0p ECTE HDF5 file
+#' @title Definition function: Determine site location and tower top measurement level from input dp0p ECTE HDF5 file
 
 #' @author 
 #' David Durden \email{ddurden@battelleecology.org}
 
 #' @description 
-#' Definition function. Function determines tower top measurement level from input dp0p ECTE HDF5 file structure
+#' Definition function. Function determines the site location and tower top measurement level from input dp0p ECTE HDF5 file structure
 
 #' @param FileInp is the input dp0p ECTE HDF5 file where the parameters are being read from.
 
-#' @return \code{LevlTowr} is returned that indicates the Horizontal and Vertical indices of the tower top measurement level. 
+#' @return \code{rpt} is list returned that indicates NEON specific site four letter code (\code{rpt$Loc})  and  the Horizontal and Vertical indices of the tower top measurement level (\code{rpt$LevlTowr}). 
 
 #' @references
 #' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007. \cr
@@ -28,33 +28,42 @@
 # changelog and author contributions / copyrights
 #   Dave Durden (2016-03-12)
 #     original creation
+#   Dave Durden (2017-09-22)
+#     adding functionality to grab Loc parameter from file structure
 
 ##############################################################################################################
 #Start of function call to determine tower top level
 ##############################################################################################################
 
-def.para.levl.towr <- function(
-  FileInp
+def.para.site <- function(
+  FileInp,
 ){
   
   if(!base::file.exists(FileInp)) {
     stop("Input file does not exist")
   } 
   
+  rpt <- list()
+  
   #Grab a list of all the data levels in the HDF5
   listPara <- rhdf5::h5ls(FileInp, datasetinfo = FALSE)
   
+  #Grab site location code parameter (Loc) from the HDF5 file structure 
+  rpt$Loc <- listPara[grepl(pattern = "^[[:upper:]]+$",x =  listPara$name), "name"]
+  
+  
   #Grab the group level with the _30m aggregation, remove that part of the string
-  LevlTowr <- sub("_30m","",unique(listPara[grep("_30m",listPara$name),"name"])) #This must be a ECTE dp0p HDF5 file to work
+  rpt$LevlTowr <- sub("_30m","",unique(listPara[grep("_30m",listPara$name),"name"])) #This must be a ECTE dp0p HDF5 file to work
   
   
   #Throw an error if length of the returned value is not 1
-  if(!base::length(LevlTowr) == 1) {
+  if(!base::length(rpt$LevlTowr) == 1|!base::length(rpt$Loc) == 1) {
     stop("Input file is not standard dp0p HDF5 structure")
     #LevlTowr <- unique(listPara[grep("000_",listPara$name),"name"]) #Second way to determine
   }
   
-  return(LevlTowr)
+  #Return output
+  return(rpt)
   
 }
   
