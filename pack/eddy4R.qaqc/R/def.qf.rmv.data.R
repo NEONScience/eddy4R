934a6719888ed43f89d88ceafbcd1ed77fd7019f
ddurden <daviddurden27@gmail.com>
2017-11-02 14:55:44 +0000

    - Updating function to output position vector of bad data points

--- pack/eddy4R.qaqc/R/def.qf.rmv.data.R
+++ pack/eddy4R.qaqc/R/def.qf.rmv.data.R
@@ -37,35 +37,49 @@ def.qf.rmv.data <- function(dfData, dfQf, Sens = NULL){
   #Create a list to hold all the output
   rpt <- list()
   
+  #Can convert to normal data.frame to prevent impact ff objects
+  #rpt$dfData<- as.data.frame(dfData)
+  
+  #Package data for reporting
   rpt$dfData <- dfData
+  
   #List of variables to check for flags to remove bad data
   rpt$listVar <- names(dfData[!names(dfData) %in% c("time", "idx")])
   
   #If a sensor (Sens) is included, check for sensor specific flags to perform filtering of data    
   if(!is.null(Sens) && length(Sens) == 1){ 
-    qfSens <- paste0("qf",toupper(substr(Sens, 1, 1)),substr(Sens,2,nchar(Sens)))
+    qfSens <- paste0("qf",toupper(substr(Sens, 1, 1)),substr(Sens,2,nchar(Sens))) # Sensor flags to look for i.e. qfIrga
   } else{
     qfSens <- NULL  
   }
   
   # Grab only qf that are integers  
   if(is.ffdf(dfQf)){
-    qfName <-  names(dfQf[vmode(dfQf) == "integer"])
+    qfName <-  names(dfQf[vmode(dfQf) == "integer"]) #method for ffdf objects
   }else{
-    qfName <-  names(Filter(is.integer, dfQf))  
+    qfName <-  names(Filter(is.integer, dfQf))  #method for normal data.frame objects
   }
   
   # A list of all the flags to be included in the data removal  
-  rpt$listFlag <- lapply(listVar, function(x){ names(dfQf[,grep(pattern = paste(x,qfSens, sep ="|"),x = qfName, ignore.case = TRUE, value = TRUE)])
+  rpt$listFlag <- lapply(rpt$listVar, function(x){ names(dfQf[,grep(pattern = paste(x,qfSens, sep ="|"),x = qfName, ignore.case = TRUE, value = TRUE)])
   })
   
+  #Add list names
+  names(rpt$listFlag) <- rpt$listVar
+  
   # Replace the flagged data with NaN, and calculate the total number of bad data points
-  rpt$numBad <-  lapply(listVar, function(x){ 
+  rpt$numBad <-  lapply(rpt$listVar, function(x){ 
+    #Subset the set of flags to be used
     tmp <- dfQf[,grep(pattern = paste(x,qfSens, sep ="|"), x = qfName, ignore.case = TRUE, value = TRUE)]
-    pos <-  which(rowSums(tmp == 1) > 0)
-    #numBad[[x]] <<- length(pos)
-    rpt$dfData[[x]][pos] <<- NaN 
-    length(pos)
+    #Record the row positions for each variable where at least 1 flag is raised
+    rpt$pos[[x]] <<-  which(rowSums(tmp == 1) > 0)
+    #Remove the data for each variable according to the position vector identified
+    rpt$dfData[[x]][rpt$pos[[x]]] <<- NaN 
+    #Calculate the total number of bad data to be removed for each variable
+    length(rpt$pos[[x]])
   })
+  
+  #Add list names
+  names(rpt$numBad) <- rpt$listVar
   
 }
