22648710e07cdc3d22f288de8d11f9beef8bd5bc
ddurden <daviddurden27@gmail.com>
2017-11-27 21:50:10 +0000

    - modularizing function to grab all thresholds passed to the function

--- pack/eddy4R.base/R/def.para.thrs.R
+++ pack/eddy4R.base/R/def.para.thrs.R
@@ -1,13 +1,15 @@
-#' @title Defination Function to read ECSE threshold table from CI-Parameter-Repo
+#' @title Definition Function to read threshold table from CI-Parameter-Repo
 
 #' @author 
 #' Natchaya Pingintha-Durden \email{ndurden@battelleecology.org}
+#' David Durden \email{ddurden@battelleecology.org}
 
 #' @description Defination Function to read ECSE threshold table from CI-Parameter-Repo
 
-#' @param \code
+#' @param dataIn data.frame containing the threshold data from the CI-Parameter-Repo
+#' @param site The site for which the parameters are being pulled
 
-#' @return
+#' @return A data.frame consisting of the threshold values to be used for the provided site.
 
 #' @references
 
@@ -25,6 +27,8 @@
 #     original creation
 #   Dave Durden (2017-06-18)
 #     modularizing and adapting for ECTE & ECSE
+#   Dave Durden (2017-11-23)
+#     modularizing the code to grab all thresholds passed in the data.frame
 ##############################################################################################
 def.para.thrs <- function(
   
@@ -32,28 +36,31 @@ def.para.thrs <- function(
   site # site name
   
 ) {
+
   #site <- c("Bart")
   dataIn <- data.frame(dataIn)
-  #get NEOn default value
+  #get NEON default value
   dataDflt <- dataIn[which(dataIn$Loc == "Neon"),]
   
+  #Check if the site is set to NEON or a specific site
   if (site %in% c("Neon")) {
-    RngMin <- dataDflt$Rng.Min
-    RngMax <- dataDflt$Rng.Max
-    DiffStepMax <- dataDflt$Step.DiffMax
-    DiffPersMin <- dataDflt$Pers.DiffMin
-    TintPers <- dataDflt$Pers.TimeDiff}
+    #Set output data to the NEON default values
+    dataOut <- dataDflt
+    }
   else {
-  dataSite <- dataIn[which(dataIn$Loc == site),]
-  
-  if (dataSite$Rng.Min %in% c("Dflt")) {RngMin <- dataDflt$Rng.Min} else {RngMin <- dataSite$Rng.Min}
-  if (dataSite$Rng.Max %in% c("Dflt")) {RngMax <- dataDflt$Rng.Max} else {RngMin <- dataSite$Rng.Max}
-  if (dataSite$Step.DiffMax %in% c("Dflt")) {DiffStepMax <- dataDflt$Step.DiffMax} else {DiffStepMax <- dataSite$Step.DiffMax}
-  if (dataSite$Pers.DiffMin %in% c("Dflt")) {DiffPersMin <- dataDflt$Pers.DiffMin} else {DiffPersMin <- dataSite$Pers.DiffMin}
-  if (dataSite$Pers.TimeDiff %in% c("Dflt")) {TintPers <- dataDflt$Pers.TimeDiff} else {TintPers <- dataSite$Pers.TimeDiff}
+  #Grab the site specific threshold values
+  dataSite <- dataIn[grep(pattern = site,x = dataIn$Loc, ignore.case = TRUE),]
+  #Check if the site specific values is set to "Dflt", then grab the default NEON-wide values
+  dataOutList <- ifelse(dataSite == "Dflt", dataDflt, dataSite) 
+  #Set the list names
+  names(dataOutList) <- names(dataSite)
+  #Convert list to dataframe
+  dataOut <- data.frame(dataOutList, stringsAsFactors = FALSE)
   }
-  
-  dataOut <- data.frame(RngMin=RngMin, RngMax=RngMax, DiffStepMax=DiffStepMax, DiffPersMin=DiffPersMin, TintPers=TintPers)
-  
-  return(dataOut)  
+  #Remove the columns not needed for the calculations
+  dataOut <- dataOut[,!names(dataOut) %in% c("Loc","LocCfgr","PrdBgn","PrdEnd")]
+  #Update the column names to the proper naming convention for dp0p HDF5 output file
+  names(dataOut) <- gsub(pattern = ".", replacement = "$", x = names(dataOut),fixed = TRUE)
+  #Return output data
+  return(dataOut) 
 }
