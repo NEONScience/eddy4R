aeede4f6140ea331de9fa092da33d62d0eb238de
ddurden <daviddurden27@gmail.com>
2017-12-09 06:44:22 +0000

    - Updating naming convention of irgaTurb flags

--- pack/eddy4R.qaqc/R/def.qf.irga.R
+++ pack/eddy4R.qaqc/R/def.qf.irga.R
@@ -9,7 +9,7 @@
 #' @param \code{diag01} The 32-bit diagnostic stream that is output from the LI7200. 
 #' @param \code{MethQf} Switch for quality flag determination for the LI7200, diag01 provides ones for passing quality by default the equals "lico". The "qfqm" method changes the ones to zeros to match the NEON QFQM logic.
 #' 
-#' @return A dataframe (\code{qfIrga}) of sensor specific irga quality flags as described in NEON.DOC.000807.
+#' @return A dataframe (\code{qfIrgaTurb}) of sensor specific irga quality flags as described in NEON.DOC.000807.
 
 #' @references 
 #' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007. \cr
@@ -30,7 +30,7 @@
 #   Dave Durden (2017-01-15)
 #     original creation
 #   Dave Durden (2017-12-02)
-#     fixing issues with missing diag data and order bits are analyzed for qfIrgaAgc
+#     fixing issues with missing diag data and order bits are analyzed for qfIrgaTurbAgc
 ##############################################################################################
 
 
@@ -49,7 +49,7 @@ def.qf.irga <- function(diag01, MethQf = c("qfqm","lico")[1]){
   posNa <- which(is.na(diag01))
   
   # Turn the diag01 into a matrix of 32 bits separated into columns for the timeseries of diagnostic values  
-qfIrga <- t(base::sapply(diag01,function(x){ base::as.integer(base::intToBits(x))}))
+qfIrgaTurb <- t(base::sapply(diag01,function(x){ base::as.integer(base::intToBits(x))}))
 
 # Function to aggregate bits to base 10 representation
 bitsToInt<-function(x) {
@@ -57,32 +57,32 @@ bitsToInt<-function(x) {
 }
 
 #Calculate the IRGA AGC value based on the first 4 bits (0-3) of the binary
-qfIrgaAgc <- base::sapply(seq_len(nrow(qfIrga)), function(x) ((bitsToInt(qfIrga[x,4:1])*6.25)+ 6.25)/100)
+qfIrgaTurbAgc <- base::sapply(seq_len(nrow(qfIrgaTurb)), function(x) ((bitsToInt(qfIrgaTurb[x,4:1])*6.25)+ 6.25)/100)
 
 #Create outpu dataframe
-qfIrga <- base::data.frame(qfIrgaAgc, qfIrga[,5:13])
+qfIrgaTurb <- base::data.frame(qfIrgaTurbAgc, qfIrgaTurb[,5:13])
 
 
 #Provide column names to the output
-base::names(qfIrga) <- c("qfIrgaAgc", "qfIrgaSync", "qfIrgaPll", "qfIrgaChop","qfIrgaDetc", "qfIrgaPres", "qfIrgaAux", "qfIrgaTempIn", "qfIrgaTempOut", "qfIrgaHead")
+base::names(qfIrgaTurb) <- c("qfIrgaTurbAgc", "qfIrgaTurbSync", "qfIrgaTurbPll", "qfIrgaTurbChop","qfIrgaTurbDetc", "qfIrgaTurbPres", "qfIrgaTurbAux", "qfIrgaTurbTempIn", "qfIrgaTurbTempOut", "qfIrgaTurbHead")
 
 
 if (MethQf == "qfqm"){
 #Change all the 1 values to 0 and 0 to 1 to fit the NEON qfqm framework
-base::lapply(base::names(qfIrga[!names(qfIrga) == "qfIrgaAgc"]), function(x) {
-  pos <- which(qfIrga[,x] == 1)
-  qfIrga[pos,x] <<- base::as.integer(0)
-  qfIrga[-pos,x] <<- base::as.integer(1)
-  qfIrga[,x] <<- base::as.integer(qfIrga[,x])
+base::lapply(base::names(qfIrgaTurb[!names(qfIrgaTurb) == "qfIrgaTurbAgc"]), function(x) {
+  pos <- which(qfIrgaTurb[,x] == 1)
+  qfIrgaTurb[pos,x] <<- base::as.integer(0)
+  qfIrgaTurb[-pos,x] <<- base::as.integer(1)
+  qfIrgaTurb[,x] <<- base::as.integer(qfIrgaTurb[,x])
 })}
 
 #Replace positions without diag data with -1
-qfIrga[posNa,] <- -1L
+qfIrgaTurb[posNa,] <- -1L
 
-qfIrga$qfIrgaAgc[posNa] <- NaN
+qfIrgaTurb$qfIrgaTurbAgc[posNa] <- NaN
 
 
 #return dataframe
-return(qfIrga)
+return(qfIrgaTurb)
 
 }
