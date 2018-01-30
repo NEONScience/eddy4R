68917cb322888a65d1f536c59c3d2c94e7e0f2d2
stefanmet <smetzger@battelleecology.org>
2018-01-30 21:59:30 +0000

    move de-spiking: wrap.prd.day.ecse.R

--- pack/eddy4R.base/R/wrap.prd.day.ecse.R
+++ pack/eddy4R.base/R/wrap.prd.day.ecse.R
@@ -2,12 +2,14 @@
 #' @title Wrapper function: To perform daily ECSE processing in native resolution
 
 #' @author
-#' Natchaya Pingintha-Durden \email{eddy4R.info@gmail.com}
+#' Natchaya Pingintha-Durden \email{eddy4R.info@gmail.com} \cr
+#' Stefan Metzger \email{eddy4R.info@gmail.com}
 
 #' @description Wrapper function. To perform daily ECSE processing in native resolution
 
-#' @param inpList List consisting of input data in the format provided by function \code{eddy4R.base::wrap.neon.read.hdf5.eddy()}. Of types numeric and integer.
-#' 
+#' @param inpList List consisting of input data in the format provided by function \code{eddy4R.base::wrap.neon.read.hdf5.eddy()}. Of types numeric, integer,  character and POSIXct.
+#' @param Desp De-spiking parameters as mixed list of types integer and character with the following list entries: \code{widt} de-spiking median filter window width (single integer); \code{nbin} de-spiking histogram bins initial number/step size (single integer); \code{rest} de-spiking resolution threshold (single integer); \code{var} sub-list of sensors in \code{inpList}, with each list entry containing the variable names for which to perform de-spiking (character). See \code{?eddy4R.qaqc::def.dspk.br86} for details.
+#'  
 #' @return 
 #' The returned object consistes of \code{rpt} after applying the daily processing.
 
@@ -26,17 +28,59 @@
 # changelog and author contributions / copyrights
 #   Natchaya Pingintha-Durden (2018-01-23)
 #     original creation
+#   Stefan Metzger (2018-01-30)
+#     move ECSE de-spiking from wrap.neon.read.hdf5.eddy() to wrap.prd.day.ecse()
 ##############################################################################################
 
 wrap.prd.day.ecse <- function(
-  inpList
+  inpList,
+  Desp
 ) {
+  
+  
   #Create a list to hold all the output
   rpt <- list()
+  
   
   #Removing high frequency flagged data
   #Applying the bad quality flags to the reported output data
   rpt <- eddy4R.qaqc::wrap.qf.rmv.data(inpList = inpList, Vrbs = FALSE, MethMeas = "ecse")
+  
+  
+  # perform de-spiking
+  # loop around sensors
+  for(sens in base::names(Desp$var)) {
+  # sens <- base::names(Desp$var)[1]
+    
+    # loop around levels
+    for(lvl in base::names(rpt$data[[sens]])) {
+    # lvl <- base::names(rpt$data[[sens]])[1]
+      
+      # loop around variables
+      for(var in Desp$var[[sens]]) {
+      # var <- Desp$var[[sens]][1]
+
+        #execute de-spiking algorithm
+        rpt$data[[sens]][[lvl]][[var]] <- eddy4R.qaqc::def.dspk.br86(
+          # input data, univariate vector of integers or numerics
+          dataIn = rpt$data[[sens]][[lvl]][[var]],
+          # filter width
+          WndwFilt = Desp$widt,
+          # initial number/step size of histogram bins
+          NumBin = Desp$nbin,
+          # resolution threshold
+          ThshReso = Desp$rest
+        )$dataOut
+     
+      }; rm(var)
+       
+    }; rm(lvl)
+    
+    # print message to screen
+    print(paste0(format(Sys.time(), "%F %T"), ": ", sens, " de-spiking complete"))
+    
+  }; rm(sens)
+  
   
   #return output
   return(rpt)
