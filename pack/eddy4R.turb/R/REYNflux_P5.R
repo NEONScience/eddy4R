5309d7d5c432d291143950a666414ff59607b1fa
stefanmet <smetzger@battelleecology.org>
2021-11-16 23:49:54 +0000

    virtual temperature

--- pack/eddy4R.turb/R/REYNflux_P5.R
+++ pack/eddy4R.turb/R/REYNflux_P5.R
@@ -124,12 +124,119 @@ REYNflux_FD_mole_dry <- function(
     densMoleH2o = data$densMoleH2o)
   base::attr(x = data$densMoleH2o, which = "unit") <- "mol m-3"
 
+  # virtual temperature [K]
+  # definition function (to be exported)
+  {
+    ##############################################################################################
+    #' @title Definition function: Calculation of the virtual temperature
+    
+    #' @author
+    #' Stefan Metzger \email{eddy4R.info@gmail.com}
+    
+    #' @description Function definition. Calculation of the virtual temperature.
+    #' The virtual temperature is the temperature of a moist air parcel at which a theoretical 
+    #' dry air parcel would have a total pressure and density equal to the moist parcel of air.
+    
+    #' @param tempAir A vector containing the air temperature, of class "numeric". [K]
+    #' @param presH2o A vector containing the water vapor partial pressure, of class "numeric". [Pa]
+    #' @param presAtm A vector containing the atmospheric pressure, of class "numeric". [Pa]
+    
+    #' @return 
+    #' The returned object is the the virtual temperature. [K]
+    
+    #' @references
+    #' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+    #' http://en.wikipedia.org/wiki/Virtual_temperature
+    
+    #' @keywords temperature, pressure, virtual
+    
+    #' @examples
+    #' Example 1, this will cause an error message due to tempAir, presH2o, presAtm have no units:
+    #' def.temp.virt.temp.air.pres.h2o.pres.atm(tempAir = 268, presH2o = 30, presAtm = 93344)
+    #' Example 2, assign values and units to variables first, the function should run ok.
+    #' tempAir = 268
+    #' presH2o = 30
+    #' presAtm = 93344
+    #' attributes(tempAir)$unit = "K"
+    #' attributes(presH2o)$unit = "Pa"
+    #' attributes(presAtm)$unit = "Pa"
+    #' def.temp.virt.temp.air.pres.h2o.pres.atm(tempAir, presH2o, presAtm)
+    
+    #' @seealso Currently none.
+    
+    #' @export
+    
+    # changelog and author contributions / copyrights
+    #   Stefan Metzger (2011-03-04)
+    #     original creation
+    ###############################################################################################
+    
+    # molar density of the mixture of dry air and water vapor
+    def.temp.virt.temp.air.pres.h2o.pres.atm <- function(
+      
+      # air temperature
+      tempAir,
+      
+      # water vapor partial pressure
+      presH2o,
+      
+      # atmospheric pressure
+      presAtm
+      
+    ) {
+      
+      # test for presence of unit attribute
+      
+      if(!("unit" %in% names(attributes(tempAir)))) {
+        
+        stop("def.temp.virt.temp.air.pres.h2o.pres.atm(): tempAir is missing unit attribute.")
+        
+      }
+      
+      
+      if(!("unit" %in% names(attributes(presH2o)))) {
+        
+        stop("def.temp.virt.temp.air.pres.h2o.pres.atm(): presH2o is missing unit attribute.")
+        
+      }
+      
+      if(!("unit" %in% names(attributes(presAtm)))) {
+        
+        stop("def.temp.virt.temp.air.pres.h2o.pres.atm(): presAtm is missing unit attribute.")
+        
+      }
+      
+      # test for correct units of input variables
+      if(attributes(tempAir)$unit != "K" || attributes(presH2o)$unit != "Pa" || attributes(presAtm)$unit != "Pa") {
+        
+        stop("def.temp.virt.temp.air.pres.h2o.pres.atm(): input units are not matching internal units, please check.")
+        
+      }
+      
+      
+      # calculate the virtual temperature
+      tempVirt <- tempAir / (1 - ((presH2o / presAtm) * (1 - eddy4R.base::IntlNatu$RtioMolmH2oDry)) )
   
-  #virtual temperature -> the temperature of a moist air parcel at which a theoretical 
-  #dry air parcel would have a total pressure and density equal to the moist parcel of air [K]
-  #http://en.wikipedia.org/wiki/Virtual_temperature
-  #    data$T_v <- data$Temp * (1 + 0.61 * data$q)
-  data$T_v <- data$tempAir / (1 - ((data$presH2o / data$presAtm) * (1 - eddy4R.base::IntlNatu$RtioMolmH2oDry)) )
+      # assign output unit
+      attributes(tempVirt)$unit <- "K"
+      
+      # return results
+      return(tempVirt) 
+      
+    }
+  }
+  # actual calculation
+  data$tempVirt <- def.temp.virt.temp.air.pres.h2o.pres.atm(
+    tempAir = data$tempAir,
+    presH2o = data$presH2o,
+    presAtm = data$presAtm)
+  
+  
+  
+  
+  
+  
+  
   #latent heat of vaporization (Eq 2.55 Foken 2008) [J kg-1] == [m2 s-2]
   data$Lv <- 2500827 - 2360 * eddy4R.base::def.unit.conv(data=as.numeric(data$tempAir),unitFrom="K",unitTo="C")
   
