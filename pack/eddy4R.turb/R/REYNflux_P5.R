494b092c4c36cd2bb82f08486d6f52f94a45bcda
stefanmet <smetzger@battelleecology.org>
2021-11-17 21:06:32 +0000

    latent heat of vaporization

--- pack/eddy4R.turb/R/REYNflux_P5.R
+++ pack/eddy4R.turb/R/REYNflux_P5.R
@@ -230,15 +230,87 @@ REYNflux_FD_mole_dry <- function(
     tempAir = data$tempAir,
     presH2o = data$presH2o,
     presAtm = data$presAtm)
+
+  # latent heat of vaporization (Eq 2.55 Foken 2008) [J kg-1] == [m2 s-2]
+  # definition function (to be exported)
+  {
+    ##############################################################################################
+    #' @title Definition function: Calculation of the latent heat of vaporization
+    
+    #' @author
+    #' Stefan Metzger \email{eddy4R.info@gmail.com}
+    
+    #' @description Function definition. Calculation of the latent heat of vaporization.
+    
+    #' @param tempAir A vector containing the air temperature, of class "numeric". [K]
+    
+    #' @return 
+    #' The returned object is the latent heat of vaporization. [J kg-1] == [m2 s-2]
+    
+    #' @references
+    #' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+    #' Foken 2008, Eq 2.55
+    
+    #' @keywords temperature, gas phase, liquid phase, heat
+    
+    #' @examples
+    #' Example 1, this will cause an error message due to tempAir has no unit:
+    #' def.heat.h2o.gas.temp(tempAir = 268)
+    #' Example 2, assign values and units to variables first, the function should run ok.
+    #' tempAir = 268
+    #' attributes(tempAir)$unit = "K"
+    #' def.heat.h2o.gas.temp(tempAir)
+    
+    #' @seealso Currently none.
+    
+    #' @export
+    
+    # changelog and author contributions / copyrights
+    #   Stefan Metzger (2011-03-04)
+    #     original creation
+    ###############################################################################################
+    
+    # latent heat of vaporization
+    def.heat.h2o.gas.temp <- function(
+      
+      # air temperature
+      tempAir
+      
+    ) {
+      
+      # test for presence of unit attribute
+      
+      if(!("unit" %in% names(attributes(tempAir)))) {
+        
+        stop("def.heat.h2o.gas.temp(): tempAir is missing unit attribute.")
+        
+      }
+      
+      # test for correct units of input variables
+      if(attributes(tempAir)$unit != "K") {
+        
+        stop("def.heat.h2o.gas.temp(): input units are not matching internal units, please check.")
+        
+      }
+      
+      
+      # calculate the latent heat of vaporization
+      heatH2oGas <- 2500827 - 2360 * eddy4R.base::def.unit.conv(data=as.numeric(tempAir), unitFrom="K", unitTo="C")
+      
+      # assign output unit
+      attributes(heatH2oGas)$unit <- "J kg-1"
+      
+      # return results
+      return(heatH2oGas) 
+      
+    }
+  }
+  # actual calculation
+  data$heatH2oGas <- def.heat.h2o.gas.temp(tempAir = data$tempAir)
+  
+
   
   
-  
-  
-  
-  
-  
-  #latent heat of vaporization (Eq 2.55 Foken 2008) [J kg-1] == [m2 s-2]
-  data$Lv <- 2500827 - 2360 * eddy4R.base::def.unit.conv(data=as.numeric(data$tempAir),unitFrom="K",unitTo="C")
   
   #-----------------------------------------------------------
   #CONSIDER HUMIDITY IN DRY ADIABATIC CONSTANT
@@ -471,7 +543,7 @@ REYNflux_FD_mole_dry <- function(
   imfl$F_LE_kin <- base$densMoleAirDry * imfl$w_hor * imfl$rtioMoleDryH2o
   mn$F_LE_kin <- mean(imfl$F_LE_kin, na.rm=TRUE)
   #latent heat flux in units of energy  [W m-2] == [kg s-3]
-  imfl$F_LE_en <- base$Lv * eddy4R.base::IntlNatu$MolmH2o * imfl$F_LE_kin
+  imfl$F_LE_en <- base$heatH2oGas * eddy4R.base::IntlNatu$MolmH2o * imfl$F_LE_kin
   mn$F_LE_en <- mean(imfl$F_LE_en, na.rm=TRUE)
   #correlation
   cor$F_LE_kin <- stats::cor(imfl$w_hor, imfl$rtioMoleDryH2o, use="pairwise.complete.obs")
