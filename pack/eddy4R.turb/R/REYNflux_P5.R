fa312d0cf0d1144bfa57d3badaae97dcb84d633f
stefanmet <smetzger@battelleecology.org>
2022-02-07 18:37:17 +0000

    create def.stat.sta.diff structure

--- pack/eddy4R.turb/R/REYNflux_P5.R
+++ pack/eddy4R.turb/R/REYNflux_P5.R
@@ -726,182 +726,270 @@ REYNflux_FD_mole_dry <- function(
   
   
   ############################################################
-  # MEANS, BASE STATES, DEVIATIONS AND SUMMARY STATISTICS
+  # SUMMARY STATISTICS, BASE STATES AND DIFFERENCES
   ############################################################
   
-  # prepare inputs
-  # reference quantities that overwrite internal calculations
-  refe <- list()
-  refe$mean$angZaxsErth <- rot$angZaxsErth
   
-  # minimum and maximum
-  
-    # min
-  
-      # calulate
-      min <- plyr::colwise("min")(data, na.rm=TRUE)
-      
-      # apply units from data to min
-      base::sapply(base::names(min), function(x) {base::attr(min[[x]], which = "unit") <<- 
-        base::attr(data[[x]], which = "unit")})
-      
-    # max
-      
-      # calulate
-      max <- plyr::colwise("max")(data, na.rm=TRUE)
-      
-      # apply units from data to max
-      base::sapply(base::names(max), function(x) {base::attr(max[[x]], which = "unit") <<- 
-        base::attr(data[[x]], which = "unit")})
-
-  
-  # always calculate means as reference
-  
-    # calculate means for euclidean (linear, cartensian) quantities
-    mean <- plyr::colwise("mean")(data, na.rm = TRUE)
-
-    # apply units from data to mean
-    base::sapply(base::names(mean), function(x) {base::attr(mean[[x]], which = "unit") <<- 
-      base::attr(data[[x]], which = "unit")})
+  # definition function (to be exported)
+  {
+    ##############################################################################################
+    #' @title Definition function: Summary statistics, base states, immediate differences
     
-    # re-calculate / re-assign means for circular (polar) quantities as vector average
+    #' @author
+    #' Stefan Metzger \email{eddy4R.info@gmail.com}
     
-      # test for correct units
+    #' @description Function definition. The function calculates summary statistics (min, max, mean), base state (mean, trend, 3rd order polynomial), and the immediate differences and standard deviations with respect to the chosen base state. This enables subsequent calculations (eddy-covariance turbulent fluxes, footprint modeling etc.) with respect to the chosen base state.
     
-        # determine if there are any variables in unit "deg"
-        tmp01 <- base::sapply(base::names(mean), function(x) {base::attr(mean[[x]], which = "unit") == "deg"})
-        
-        # stop and print error message to screen
-        if(length(which(tmp01)) > 0) {
-          stop(base::paste0("my.function(): ", base::names(base::which(tmp01)), 
-                            " units are not matching internal units (radians), please check."))}
-        
-        # clean up
-        base::rm(tmp01)
+    #' @param inp A data frame containing the wind vector in meteorological convention with the variables veloXaxs (latitudinal wind speed, positive from west), veloYaxs (longitudinal wind speed, positive from south), and veloZaxs (vertical wind speed, positive from below) of class "numeric, each with unit attribute. [m s-1]
+    
+    #' @return 
+    #' The returned object is a list containing the elements data and rot.
+    #' data is a dataframe with the same number of observations as the function call inputs. It contains the wind vector variables in streamwise convention veloXaxsHor (streamwise wind speed, positive from front), veloYaxsHor (cross-wind speed, positive from left) and veloZaxsHor (vertical wind speed, positive from below) [m s-1], and the wind direction angZaxsErth [rad], each of class "numeric and with unit attribute.
+    #' rot is a list with the objects used in the rotation, averaged over all observations in the function call inputs. It contains the mean wind direction angZaxsErth [rad], the resulting rotation matrix mtrxRot01 [-] and the transpose of the rotation matrix mtrxRot02 [-], each of class "numeric and with unit attribute. It should be noted that angZaxsErth is calculated by first averaging each horizontal component of the wind vector, which minimizes the mean cross-wind thus satisfying conditions for footprint modeling and separating shear into stream-wise and cross-wind terms.
+    
+    #' @references
+    #' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+    
+    #' @keywords rotation, stream-wise, mean wind, footprint, shear
+    
+    #' @examples
+    #' Example 1, this will cause an error message due to inp01$veloYaxs is missing:
+    #' inp01 <- base::data.frame(
+    #'   veloXaxs = c(-1.889635, -1.661724, -1.615837, -1.711132, -1.223001),
+    #'   veloZaxs = c(0.176613897, 0.184947662, 0.344331819, 0.190230311, 0.239193186)
+    #' )
+    #' attr(inp01$veloXaxs,"unit") <- "m s-1"; attr(inp01$veloZaxs,"unit") <- "m s-1"
+    #' def.rot.ang.zaxs.erth(inp = inp01)
+    #' base::rm(inp01)
+    #' Example 2, make sure to assign all variables and units, the function should run ok.
+    #' inp02 <- base::data.frame(
+    #'   veloXaxs = c(-1.889635, -1.661724, -1.615837, -1.711132, -1.223001),
+    #'   veloYaxs = c(1.365195, 1.277106, 1.394891, 1.180698, 1.283836),
+    #'   veloZaxs = c(0.176613897, 0.184947662, 0.344331819, 0.190230311, 0.239193186)
+    #' )
+    #' attr(inp02$veloXaxs,"unit") <- "m s-1"; attr(inp02$veloYaxs,"unit") <- "m s-1"; attr(inp02$veloZaxs,"unit") <- "m s-1"
+    #' out02 <- def.rot.ang.zaxs.erth(inp = inp02)
+    #' utils::str(out02)
+    #' base::rm(inp02, out02)
+    
+    #' @seealso Currently none.
+    
+    #' @export
+    
+    # changelog and author contributions / copyrights
+    #   Stefan Metzger (2011-03-04)
+    #     original creation
+    #   Stefan Metzger (2022-02-07)
+    #     update to eddy4R terminology and modularize into definition function
+    ###############################################################################################
+    
+    
+    # rotation into the mean wind
+    def.stat.sta.diff <- function(
       
-      # determine whether there are any variables in unit "rad"
-      tmp02 <- base::sapply(base::names(mean), function(x) {base::attr(mean[[x]], which = "unit") == "rad"})
+      # input dataframe containing the variables for which to perform the calculations, each with unit attribute
+      inp, # <- data
       
-      # continue only if there is at least one variable in unit "rad"
-      if(length(which(tmp02)) > 0) {
-        
-        # variables to re-calculate / re-assign
-        tmp03 <- base::names(base::which(tmp02))
-        
-        # re-calculate / re-assign
-        for(idx in tmp03) {
-          
-          # re-assign reference value if provided
-          if(!is.null(refe$mean) & idx %in% base::names(refe$mean)) {
-            
-            mean[[idx]] <- refe$mean[[idx]]
-          
-          # re-calculate otherwise
-          # need to change from degree to internal units radians: eddy4R.base::def.pol.cart() and 
-          # eddy4R.base::def.cart.pol(); inconsistent results when calculating mean wind direction
-          # 1) directly from mean horizontal wind vector [m s-1] via single call to eddy4R.base::def.pol.cart() vs.
-          # 2) re-calculating from high-frequency wind direction -> unit vector via call to 
-          # eddy4R.base::def.pol.cart(eddy4R.base::def.cart.pol()); implemented above look-back as a
-          # workaround specifically for wind direction to be consistently defined as the direction that
-          # minimizes the mean cross-wind; example:
-          # 124.8 deg from 1) wind vector: eddy4R.base::def.pol.cart(cart = base::matrix(c(0.9941063, -1.429056), ncol=2))
-          # 123.6 deg from 2) unit vector: eddy4R.base::def.pol.cart(cart = base::matrix(c(0.5394431, -0.8106568), ncol=2))
-          } else {
-          
-            mean[[idx]] <- eddy4R.base::def.unit.conv(data = eddy4R.base::def.pol.cart(
-              cart = base::matrix(
-                base::colMeans(
-                  eddy4R.base::def.cart.pol(
-                    az = eddy4R.base::def.conv.poly(data = data[[idx]], coefPoly = eddy4R.base::IntlConv$RadDeg)
-                    ),
-                  na.rm=TRUE),
-                ncol=2)
-              ), unitFrom = "deg", unitTo = "rad")
+      # list of reference quantities that can be supplied to overwrite internal calculations
+      # currently implemented only for refe$mean in combination with inp variables in units [rad]
+      refe # <- base::list("mean" = base::list("angZaxsErth" = rot$angZaxsErth))
+    ) {
 
-          } 
-          
-        }
-        base::rm(idx)
-        
+      # check that input is of class data.frame
+      if(base::class(inp) != "data.frame") {
+        stop(base::paste0("def.stat.sta.diff(): inp is not of class data.frame, please check."))  
       }
-      # clean up
-      base::rm(tmp02, tmp03)
-
-      
-  # base states; AlgBase <- c("mean", "trnd", "ord03")[2]
-      
-      # re-assign mean data if AlgBase == "mean"
-      if(AlgBase == "mean") {
-        
-        base <- mean
-        
-      # compute base state otherwise
-      } else {
-        
-        # calculate base states      
-        base <- base::sapply(base::names(data), function(x) eddy4R.base::def.base.ec(
-          idxTime = data$idep,
-          var = data[[x]],
-          AlgBase = AlgBase),
-          simplify = FALSE)
-        
-        # reshape results
-        base <- base::as.data.frame(base::matrix(base::unlist(base), ncol = base::ncol(data)))
-        base::attributes(base)$names <- base::attributes(data)$names
-        
-        # apply units from data to base
-        base::sapply(base::names(base), function(x) {base::attr(base[[x]], which = "unit") <<- 
-          base::attr(data[[x]], which = "unit")})
-        
-        # determine whether there are any variables in unit "rad"
-        tmp01 <- base::sapply(base::names(base), function(x) {base::attr(base[[x]], which = "unit") == "rad"})
-        
-        # continue only if there is at least one variable in unit "rad"
-        if(length(which(tmp01)) > 0) {
+    
+        # test input variables and unit attributes
+        for(idx in base::names(inp)){
+          # idx <- "veloXaxs"
           
-          # variables to re-calculate / re-assign
-          tmp02 <- base::names(base::which(tmp01))
+          # test for presence/absence of unit attribute
+          if(!("unit" %in% base::names(attributes(inp[[idx]])))) {
+            stop(base::paste0("def.rot.ang.zaxs.erth(): inp$", idx, " is missing unit attribute."))        }
+      
+        }; base::rm(idx)
+    
+      
+      # minimum and maximum
+      
+        # min
+      
+          # calculate
+          min <- plyr::colwise("min")(inp, na.rm=TRUE)
           
-          # assign NAs - current computation of de-trending or 3rd order polynomial not defined for circular quantities
-          for(idx in tmp02) {
+          # apply units from inp to min
+          base::sapply(base::names(min), function(x) {base::attr(min[[x]], which = "unit") <<- 
+            base::attr(inp[[x]], which = "unit")})
+          
+        # max
+          
+          # calculate
+          max <- plyr::colwise("max")(inp, na.rm=TRUE)
+          
+          # apply units from inp to max
+          base::sapply(base::names(max), function(x) {base::attr(max[[x]], which = "unit") <<- 
+            base::attr(inp[[x]], which = "unit")})
+    
+      
+      # always calculate means as reference
+      
+        # calculate means for euclidean (linear, cartensian) quantities
+        mean <- plyr::colwise("mean")(inp, na.rm = TRUE)
+    
+        # apply units from inp to mean
+        base::sapply(base::names(mean), function(x) {base::attr(mean[[x]], which = "unit") <<- 
+          base::attr(inp[[x]], which = "unit")})
+        
+        # re-calculate / re-assign means for circular (polar) quantities as vector average
+        
+          # test for correct units
+        
+            # determine if there are any variables in unit "deg"
+            tmp01 <- base::sapply(base::names(mean), function(x) {base::attr(mean[[x]], which = "unit") == "deg"})
             
-            base[[idx]] <- base::rep(x = NaN, length.out = base::length(base[[idx]]))
-            attributes(base[[idx]])$unit <- attributes(data[[idx]])$unit
+            # stop and print error message to screen
+            if(length(which(tmp01)) > 0) {
+              stop(base::paste0("def.stat.sta.diff(): ", base::names(base::which(tmp01)), 
+                                " units are not matching internal units (radians), please check."))}
+            
+            # clean up
+            base::rm(tmp01)
+          
+          # determine whether there are any variables in unit "rad"
+          tmp02 <- base::sapply(base::names(mean), function(x) {base::attr(mean[[x]], which = "unit") == "rad"})
+          
+          # continue only if there is at least one variable in unit "rad"
+          if(length(which(tmp02)) > 0) {
+            
+            # variables to re-calculate / re-assign
+            tmp03 <- base::names(base::which(tmp02))
+            
+            # re-calculate / re-assign
+            for(idx in tmp03) {
+              
+              # re-assign reference value if provided
+              if(!is.null(refe$mean) & idx %in% base::names(refe$mean)) {
+                
+                # test for correct units
+                if(attributes(refe$mean[[idx]])$unit != attributes(inp[[idx]])$unit) {
+                  stop(base::paste0("def.stat.sta.diff(): refe$mean$", idx, 
+                                    " unit is not matching inp$", idx, " unit, please check."))}
+                
+                # re-assign
+                mean[[idx]] <- refe$mean[[idx]]
+              
+              # re-calculate otherwise
+              # need to change from degree to internal units radians: eddy4R.base::def.pol.cart() and 
+              # eddy4R.base::def.cart.pol(); inconsistent results when calculating mean wind direction
+              # 1) directly from mean horizontal wind vector [m s-1] via single call to eddy4R.base::def.pol.cart() vs.
+              # 2) re-calculating from high-frequency wind direction -> unit vector via call to 
+              # eddy4R.base::def.pol.cart(eddy4R.base::def.cart.pol()); implemented above look-back as a
+              # workaround specifically for wind direction to be consistently defined as the direction that
+              # minimizes the mean cross-wind; example:
+              # 124.8 deg from 1) wind vector: eddy4R.base::def.pol.cart(cart = base::matrix(c(0.9941063, -1.429056), ncol=2))
+              # 123.6 deg from 2) unit vector: eddy4R.base::def.pol.cart(cart = base::matrix(c(0.5394431, -0.8106568), ncol=2))
+              } else {
+              
+                mean[[idx]] <- eddy4R.base::def.unit.conv(data = eddy4R.base::def.pol.cart(
+                  cart = base::matrix(
+                    base::colMeans(
+                      eddy4R.base::def.cart.pol(
+                        az = eddy4R.base::def.conv.poly(data = data[[idx]], coefPoly = eddy4R.base::IntlConv$RadDeg)
+                        ),
+                      na.rm=TRUE),
+                    ncol=2)
+                  ), unitFrom = "deg", unitTo = "rad")
+    
+              } 
+              
+            }; base::rm(idx)
             
           }
-          base::rm(idx)
+          # clean up
+          base::rm(tmp02, tmp03)
+    
           
-        }
-        # clean up
-        base::rm(tmp01, tmp02)
-      
-      }
-
-      
-  # immediate fluctuations (corresponding to base state treatment)
-      
-    # calculate
-    diff <- sapply(base::names(data), function(x) data[[x]] - base[[x]])
+      # base states; AlgBase <- c("mean", "trnd", "ord03")[2]
+          
+          # re-assign mean data if AlgBase == "mean"
+          if(AlgBase == "mean") {
+            
+            base <- mean
+            
+          # compute base state otherwise
+          } else {
+            
+            # calculate base states      
+            base <- base::sapply(base::names(inp), function(x) eddy4R.base::def.base.ec(
+              idxTime = inp$idep,
+              var = inp[[x]],
+              AlgBase = AlgBase),
+              simplify = FALSE)
+            
+            # reshape results
+            base <- base::as.data.frame(base::matrix(base::unlist(base), ncol = base::ncol(inp)))
+            base::attributes(base)$names <- base::attributes(inp)$names
+            
+            # apply units from inp to base
+            base::sapply(base::names(base), function(x) {base::attr(base[[x]], which = "unit") <<- 
+              base::attr(inp[[x]], which = "unit")})
+            
+            # determine whether there are any variables in unit "rad"
+            tmp01 <- base::sapply(base::names(base), function(x) {base::attr(base[[x]], which = "unit") == "rad"})
+            
+            # continue only if there is at least one variable in unit "rad"
+            if(length(which(tmp01)) > 0) {
+              
+              # variables to re-calculate / re-assign
+              tmp02 <- base::names(base::which(tmp01))
+              
+              # assign NAs - current computation of de-trending or 3rd order polynomial not defined for circular quantities
+              for(idx in tmp02) {
+                
+                base[[idx]] <- base::rep(x = NaN, length.out = base::length(base[[idx]]))
+                attributes(base[[idx]])$unit <- attributes(inp[[idx]])$unit
+                
+              }
+              base::rm(idx)
+              
+            }
+            # clean up
+            base::rm(tmp01, tmp02)
+          
+          }
     
-    # reshape data
-    diff <- base::as.data.frame(base::matrix(diff, ncol = base::ncol(data)))
-    base::attributes(diff)$names <- base::attributes(data)$names
+          
+      # immediate differences (corresponding to chosen base state treatment)
+          
+        # calculate
+        diff <- sapply(base::names(inp), function(x) inp[[x]] - base[[x]])
+        
+        # reshape data
+        diff <- base::as.data.frame(base::matrix(diff, ncol = base::ncol(inp)))
+        base::attributes(diff)$names <- base::attributes(inp)$names
+        
+        # apply units from inp to diff
+        base::sapply(base::names(diff), function(x) {base::attr(diff[[x]], which = "unit") <<- 
+          base::attr(inp[[x]], which = "unit")})
     
-    # apply units from data to diff
-    base::sapply(base::names(diff), function(x) {base::attr(diff[[x]], which = "unit") <<- 
-      base::attr(data[[x]], which = "unit")})
+          
+      # standard deviations (corresponding to chosen base state treatment)
+        
+        # calculate
+        sd <- plyr::colwise("sd")(diff, na.rm=TRUE)
+        
+        # apply units from inp to sd
+        base::sapply(base::names(sd), function(x) {base::attr(sd[[x]], which = "unit") <<- 
+          base::attr(inp[[x]], which = "unit")})
+    
+    
+    
+    }
   
-  # standard deviations (corresponding to base state treatment)
-    
-    # calculate
-    sd <- plyr::colwise("sd")(diff, na.rm=TRUE)
-    
-    # apply units from data to sd
-    base::sapply(base::names(sd), function(x) {base::attr(sd[[x]], which = "unit") <<- 
-      base::attr(data[[x]], which = "unit")})
-    
-    
-
+  }
+  
+  
 
     
   
