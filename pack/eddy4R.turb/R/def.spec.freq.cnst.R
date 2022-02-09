afccd406100c92f790737fb235972d5684bd6f49
ddurden <daviddurden27@gmail.com>
2022-02-09 15:00:24 +0000

    - terms update for eddy4R.turb

--- pack/eddy4R.turb/R/def.spec.freq.cnst.R
+++ pack/eddy4R.turb/R/def.spec.freq.cnst.R
@@ -1,23 +1,26 @@
 ##############################################################################################
 #' @title Definition function: 63\% frequency constant after Aubinet (2012) Eq. 4.22
 
-# type (one of function defintion, function wrapper, workflow, demo): function defintion
+# type (one of function definition, function wrapper, workflow, demo): function definition
 
 # license: Terms of use of the NEON FIU algorithm repository dated 2015-01-16
 
 #' @author Stefan Metzger \email{eddy4R.info@gmail.com}
+#' 
 
 # changelog and author contributions / copyrights
 #   Stefan Metzger (2014-09-22)
 #     original creation
 #   Stefan Metzger (2015-11-28)
 #     re-formualtion as function() to allow packaging
+#   David Durden (2022-02-09)
+#     terms update
 
 #' @description 63\% frequency constant after Aubinet (2012) Eq. 4.22.
 
-#' @param Currently none
+#' @param inpPrd numeric, time period in seconds
 
-#' @return Currently none
+#' @return A numeric vector \code{freqOut} of equivalent frequencies to the input time periods
 
 #' @references Currently none
 
@@ -30,4 +33,12 @@
 #' @export
 ##############################################################################################
 
-const_f <- function(t0) 1 / (2 * pi * t0)
+def.spec.freq.cnst <- function(inpPrd) {
+  
+  #Convert time/period constant values to frequency
+  freqOut <- 1 / (2 * pi * inpPrd)
+  
+  #Return output
+  return(freqOut)
+  
+}#End function
