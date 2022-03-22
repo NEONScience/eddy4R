1351bc9aadde3e655d7e942af822ebbabd1f0390
stefanmet <smetzger@battelleecology.org>
2022-03-22 23:12:53 +0000

    modularized volumentric heat capacity

--- pack/eddy4R.turb/R/REYNflux_P5.R
+++ pack/eddy4R.turb/R/REYNflux_P5.R
@@ -266,6 +266,99 @@ REYNflux_FD_mole_dry <- function(
     presH2o = data$presH2o,
     presAtm = data$presAtm)
 
+  # volumentric heat capacity [kg m-1 s-2 K-1] = specific heat [J kg-1 K-1] x mole density [mol m-3] x mole mass [kg mol-1]
+  # definition function (to be exported)
+  {
+    ##############################################################################################
+    #' @title Definition function: Calculation of the volumetric heat capacity
+    
+    #' @author
+    #' Stefan Metzger \email{eddy4R.info@gmail.com}
+    
+    #' @description Function definition. Calculation of the volumetric heat capacity
+    
+    #' @param densMoleAirDry A vector containing the dry air mole density, of class "numeric". [mol m-3]
+    #' @param densMoleH2o A vector containing the H2O mole density, of class "numeric". [mol m-3]
+    
+    #' @return 
+    #' The returned object is the volumetric heat capacity. [kg m-1 s-2 K-1] = specific heat [J kg-1 K-1] x mole density [mol m-3] x mole mass [kg mol-1]
+    
+    #' @references
+    #' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+    
+    #' @keywords sensible heat flux, temperature, heat
+    
+    #' @examples
+    #' Example 1, this will cause an error message due to densMoleAirDry, densMoleH2o have no units:
+    #' def.heat.air.wet(densMoleAirDry = 41.9054, densMoleH2o = 0.01342256)
+    #' Example 2, assign values and units to variables first, the function should run ok.
+    #' densMoleAirDry = 41.9054; attributes(densMoleAirDry)$unit <- "mol m-3"
+    #' densMoleH2o = 0.01342256; attributes(densMoleH2o)$unit <- "mol m-3"
+    #' def.heat.air.wet(densMoleAirDry, densMoleH2o)
+
+    #' @seealso Currently none.
+    
+    #' @export
+    
+    # changelog and author contributions / copyrights
+    #   Stefan Metzger (2011-03-04)
+    #     original creation
+    #   Stefan Metzger (2022-03-22)
+    #     update to eddy4R terminology and modularize into definition function
+    ###############################################################################################
+    
+    # volumetric heat capacity
+    def.heat.air.wet <- function(
+
+      # dry air mole density
+      densMoleAirDry,
+
+      # H2O mole density
+      densMoleH2o
+      
+    ) {
+      
+      # test for presence of unit attribute
+      
+        # densMoleAirDry
+        if(!("unit" %in% names(attributes(densMoleAirDry)))) {
+          
+          stop("def.heat.air.wet(): densMoleAirDry is missing unit attribute.")
+          
+        }
+
+        # densMoleH2o
+        if(!("unit" %in% names(attributes(densMoleH2o)))) {
+          
+          stop("def.heat.air.wet(): densMoleH2o is missing unit attribute.")
+          
+        }
+      
+      # test for correct units of input variables
+      if(attributes(densMoleAirDry)$unit != "mol m-3" | attributes(densMoleH2o)$unit != "mol m-3") {
+        
+        stop("def.heat.air.wet(): input units are not matching internal units, please check.")
+        
+      }
+      
+      # calculate the volumetric heat capacity
+      heatAirWet <- 
+        # dry air 
+        eddy4R.base::IntlNatu$CpDry * densMoleAirDry * eddy4R.base::IntlNatu$MolmDry + 
+        # water vapor  
+        eddy4R.base::IntlNatu$CpH2o * densMoleH2o * eddy4R.base::IntlNatu$MolmH2o    
+      
+      # assign output unit
+      base::attr(heatAirWet, which = "unit") <- "kg m-1 s2 K-1"
+      
+      # return results
+      base::return(heatAirWet)
+      
+    }
+  }
+  # actual calculation
+  data$heatAirWet <- def.heat.air.wet(densMoleAirDry = data$densMoleAirDry, densMoleH2o = data$densMoleH2o)
+  
   # latent heat of vaporization (Eq 2.55 Foken 2008) [J kg-1] == [m2 s-2]
   # definition function (to be exported)
   {
@@ -1292,6 +1385,8 @@ REYNflux_FD_mole_dry <- function(
     rot = rot,
     Unit = base::data.frame(In = "m s-1", Out = "m s-1", OutSq = "m2 s-2")
   )
+  
+  # assignment to statStaDiff similar to def.flux.sclr below
 
   
   
@@ -1378,27 +1473,7 @@ REYNflux_FD_mole_dry <- function(
       
   }
   
-  
-  # define inputs
-  
-    # move to pre-calculation similar to base$heatH2oGas
-    # volumentric heat capacity [kg m-1 s-2 K-1] =
-    # specific heat [J kg-1 K-1] x mole density [mol m-3] x mole mass [kg mol-1]
-    heatAirWet <- 
-      # dry air 
-      eddy4R.base::IntlNatu$CpDry * statStaDiff$base$densMoleAirDry * eddy4R.base::IntlNatu$MolmDry + 
-      # water vapor  
-      eddy4R.base::IntlNatu$CpH2o * statStaDiff$base$densMoleH2o * eddy4R.base::IntlNatu$MolmH2o    
-    
-    # assign units
-    base::attr(heatAirWet, which = "unit") <- "kg m-1 s2 K-1"
-    
-    
-  # check how the fluxes are currently called in the h5 file
-  # fluxH2o, fluxH2oEngy, fluxH2OEvtr
-  # fluxCo2
 
-  
   # initiate dataframe to store correlations
   statStaDiff$corr <- data.frame(fluxTemp = NaN)
 
@@ -1416,7 +1491,7 @@ REYNflux_FD_mole_dry <- function(
     # sensible heat flux in units of energy [kg s-3] = [W m-2]
     fluxTmp <- def.flux.sclr(
       inp = data.frame(vect = statStaDiff$diff$veloZaxsHor, sclr = statStaDiff$diff$tempAir),
-      conv = heatAirWet,
+      conv = statStaDiff$base$heatAirWet,
       Unit = base::data.frame(InVect = "m s-1", InSclr = "K", Conv = "kg m-1 s2 K-1", Out = "W m-2")
     )
     for(idx in base::names(fluxTmp)) statStaDiff[[idx]]$fluxTempEngy <- fluxTmp[[idx]]
