e39a70adad283bc24d8ee74d00117e8fe10e1794
daviddurden <daviddurden27@gmail.com>
2022-04-19 16:18:17 +0000

    - updating goldfile links to GCS

--- pack/eddy4R.base/R/def.hdf5.crte.R
+++ pack/eddy4R.base/R/def.hdf5.crte.R
@@ -104,7 +104,7 @@ def.hdf5.crte <- function(
 
     DirTmp <- tempdir()
     #Download file description readme and object list  
-    eddy4R.base::def.dld.zip(Inp = list(Url = "https://s3.data.neonscience.org/neon-ec-goldfiles/EC-turbulence-processing/fileDesc.zip", Dir = DirTmp))
+    eddy4R.base::def.dld.zip(Inp = list(Url = "https://storage.googleapis.com/neon-ec-goldfiles/EC-turbulence-processing/fileDesc.zip", Dir = DirTmp))
     
     #Store the path to the readme file
     FileNameReadMe <- base::list.files( path = base::paste0(DirTmp,"/fileDesc"), pattern = ".txt", full.names = TRUE)
