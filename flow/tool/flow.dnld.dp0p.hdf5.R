c0a09ec1d06391ff4be8ed67f37eef6531ec082b
ddurden <daviddurden27@gmail.com>
2023-02-14 21:27:16 +0000

    - updating url

--- flow/tool/flow.dnld.dp0p.hdf5.R
+++ flow/tool/flow.dnld.dp0p.hdf5.R
@@ -47,7 +47,7 @@ PrdWndwDnld <- base::as.character(DateSeq)
 fileInBase <- paste0("NEON.",dom,".",site,".IP0.00200.001.",sys,".")
 
 #Create URL for data files
-urlDnld <- paste0("https://storage.cloud.google.com/neon-sae-files/ods/dataproducts/IP0/",PrdWndwDnld,"/",site,"/",fileInBase,PrdWndwDnld,".l0p.h5.gz")
+urlDnld <- paste0("https://storage.googleapis.com/neon-sae-files/ods/dataproducts/IP0/",PrdWndwDnld,"/",site,"/",fileInBase,PrdWndwDnld,".l0p.h5.gz")
 
 #Download filename (full path)
 fileDnld <-  paste0(DirDnld,"/",base::toupper(sys),"_dp0p_",site,"_",PrdWndwDnld,".h5.gz")
