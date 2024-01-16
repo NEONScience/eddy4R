0d0dca27590adbf00555ca259b6e84d1246cfed3
ddurden <daviddurden27@gmail.com>
2024-01-16 19:51:04 +0000

    - adding argument for Token

--- pack/eddy4R.base/R/wrap.hdf5.wrte.dp01.api.R
+++ pack/eddy4R.base/R/wrap.hdf5.wrte.dp01.api.R
@@ -35,6 +35,8 @@
 #     applied term name convention; replace Levl by Lvl
 #   Natchaya P-Durden (2019-06-10)
 #     adding additional data products
+#   David Durden (2024-01-16)
+#     Adding token to API calls and logging
 ##############################################################################################
 
 # date <- "20170901"
@@ -55,7 +57,8 @@ wrap.hdf5.wrte.dp01.api <- function(
   SiteLoca,
   DpName = c("tempAirLvl", "tempAirTop", "fluxHeatSoil", "radiNet", "tempSoil", "h2oSoilVol", "presBaro"),
   LvlTowr,
-  TimeAgr = c(1,30)
+  TimeAgr = c(1,30),
+  Tokn = NULL 
 ){
 
   #Initialize the reporting data list
@@ -65,11 +68,11 @@ wrap.hdf5.wrte.dp01.api <- function(
  for(idxDp in DpName){
    #print screen
    msg<-paste0("Begin to re-ingest ", idxDp, " dp01 data")
-   tryCatch({rlog$debug(msg)}, error=function(cond){print(msg)})
+   tryCatch({rlog$info(msg)}, error=function(cond){print(msg)})
    #Call the definition function for all the data product
    rpt[[idxDp]] <- lapply(TimeAgr, function(x) {
      #Use the definition function to grab reingest data
-     eddy4R.base::def.hdf5.wrte.dp01.api(date = date, SiteLoca = SiteLoca, FileOut = FileOut, DpName = idxDp, LvlTowr = LvlTowr, TimeAgr = x)
+     eddy4R.base::def.hdf5.wrte.dp01.api(date = date, SiteLoca = SiteLoca, FileOut = FileOut, DpName = idxDp, LvlTowr = LvlTowr, TimeAgr = x, Tokn = Tokn)
      })# End of lapply function
    } #End of for loop around dp01 data products
 
