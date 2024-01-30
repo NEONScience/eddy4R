4f152a177d6eb3d668e3b913770c62c2c18d863b
sambower-atmo <sbower@atmofacts.com>
2024-01-30 23:27:55 +0000

    Add header

--- pack/eddy4R.maps/R/def.plot.flux.spatial.R
+++ pack/eddy4R.maps/R/def.plot.flux.spatial.R
@@ -1,4 +1,44 @@
+##############################################################################################
+#' @title Definition function: Plot fluxes on a basemap
 
+#' @author
+#' Sam Bower \email{sbower@atmofacts.com}
+
+#' @description Function definition. This function saves a GeoTiff flux map onto a static basemap or an interactive html map.
+#' 
+#' @param input_path A spatial file or folder of spatial files to plot on the basemap.
+#' @param output_path A string for the folder location to save output maps.
+#' @param interactive If FALSE - plots the flux(es) onto a static basemap. If TRUE, plots the flux(es) onto an interactive basemap.
+#' @param basemap_style For a list of basemap styles use names(providers)
+
+#' @return 
+#' 
+#' 
+
+#' @references
+#' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+
+#' @keywords visualization, flux mapping
+#' 
+#' @examples
+#' 
+#' path_in = "/path/to/folder/of/tif/files"
+#' path_out = "/path/to/desired/output/location"
+#' plot.flux.spatial(path_in, path_out, 'viridis', opacity = 0.5,'OpenStreetMap', TRUE)
+#' 
+
+
+
+#' @seealso Currently none.
+
+#' @export
+
+# changelog and author contributions / copyrights
+#   Stefan Metzger (2011-03-04)
+#     original creation
+#   Stefan Metzger (2022-02-08)
+#     update to eddy4R terminology and modularize into definition function
+###############################################################################################
 # Load necessary libraries
 #library(ggplot2)
 #library(ggmap)
@@ -7,7 +47,7 @@
 #library(mapview)
 
 # Define the function with improved interactive HTML handling
-plot_geotiff_on_basemap <- function(input_path, output_path, colormap = 'viridis', opacity = 0.5, basemap_style = 'OpenStreetMap', interactive = FALSE) {
+plot.flux.spatial <- function(input_path, output_path, colormap = 'viridis', opacity = 0.5, basemap_style = 'OpenStreetMap', interactive = FALSE) {
   # Check if input_path is a directory or a single file
   if (dir.exists(input_path)) {
     # Handle directory of TIFF files
@@ -62,8 +102,3 @@ plot_geotiff_on_basemap <- function(input_path, output_path, colormap = 'viridis
     }
   }
 }
-
-# Example usage:
-path_in = "/home/sbower/eddy/sambower/Documents/AtmoFacts/maps_package/test_data/random_geotiff/footprints/"
-path_out = "/home/sbower/eddy/sambower/Documents/AtmoFacts/maps_package/test_data/random_geotiff/outputs/"
-plot_geotiff_on_basemap(path_in, path_out, 'viridis', opacity = 0.5,'OpenStreetMap', TRUE)
