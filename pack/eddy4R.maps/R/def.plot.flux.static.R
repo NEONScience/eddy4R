6a4f6e08560cadc6d713bc35605d0aa31bcd4e5d
sambower-atmo <sbower@atmofacts.com>
2024-02-01 21:32:44 +0000

    Create def.plot.flux.static framework.

--- pack/eddy4R.maps/R/def.plot.flux.static.R
+++ pack/eddy4R.maps/R/def.plot.flux.static.R
@@ -1,1 +1,28 @@
-def.plot.flux.static
+
+
+
+
+def.plot.flux.static <- function(inputPath, basemapStyle = 'osm', nodataValue = 0, outputPath = 'output') {
+
+# Change java system parameters to allow for headless operation (to disable under-the-hood java GUI which will not work in this Docker container) . 
+  options(java.parameters = "-Djava.awt.headless=true")
+  
+# Load required libraries.
+  
+# read in single raster file.
+  
+# set nodata value.
+  
+# create basemap layer with OpenStreetMap and tmaptools.
+  
+# superimpose raster layer with given color scheme.
+  
+# save
+  
+# Read in folder of raster files
+  
+# Plot iteratively 
+  
+# Save
+}
+
