8ffcd929e785ad65965fe1b709afe14a86b5a6c2
sambower-atmo <sbower@atmofacts.com>
2024-02-01 21:40:31 +0000

    Check for files in input.

--- pack/eddy4R.maps/R/def.plot.flux.static.R
+++ pack/eddy4R.maps/R/def.plot.flux.static.R
@@ -1,16 +1,18 @@
 
 
-
-
 def.plot.flux.static <- function(inputPath, basemapStyle = 'osm', nodataValue = 0, outputPath = 'output') {
 
 # Change java system parameters to allow for headless operation (to disable under-the-hood java GUI which will not work in this Docker container) . 
   options(java.parameters = "-Djava.awt.headless=true")
   
-# Load required libraries.
-  
 # read in single raster file.
   
+  if (dir.exists(inputPath)) {
+    rasterFiles <- list.files(inputPath, pattern = "\\.tif$", full.names = TRUE)
+    if (length(rasterFiles) == 0) {
+      stop("No raster files found in the directory.")
+    }
+    
 # set nodata value.
   
 # create basemap layer with OpenStreetMap and tmaptools.
@@ -24,5 +26,5 @@ def.plot.flux.static <- function(inputPath, basemapStyle = 'osm', nodataValue = 
 # Plot iteratively 
   
 # Save
-}
+}}
 
