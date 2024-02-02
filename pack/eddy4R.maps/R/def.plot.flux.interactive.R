380338b42964c09fd86b50cf1b8f2433a38dd427
sambower-atmo <sbower@atmofacts.com>
2024-02-02 16:41:23 +0000

    Add save functionality.

--- pack/eddy4R.maps/R/def.plot.flux.interactive.R
+++ pack/eddy4R.maps/R/def.plot.flux.interactive.R
@@ -1,9 +1,12 @@
 def.plot.flux.interactive <- function(
   input_path,
   nodata_value = 0,
-  alpha = 0.7,  # Use specified alpha
-  colormap = 'YlOrRd'
+  alpha = 0.7,
+  colormap = 'YlOrRd',
+  save_path = NULL
 ) {
+  
+  
   # Determine if input_path is a directory or a single file
   if (dir.exists(input_path)) {
     rasterFiles <- list.files(input_path, pattern = "\\.tif$", full.names = TRUE)
@@ -43,6 +46,10 @@ def.plot.flux.interactive <- function(
   # Add layers control to the map
   map <- map %>% addLayersControl(overlayGroups = rasterGroups, baseGroups = c("Street", "Imagery", "Dark"))
   
-  # Print the map
-  print(map)
+  if (!is.null(save_path)) {
+    saveWidget(map, file = save_path, selfcontained = TRUE)
+  } else {
+    # Print the map to display it interactively
+    print(map)
+  }
 }
