513f9ddf7d10445eec2343d0951618e77f053b41
sambower-atmo <sbower@atmofacts.com>
2024-02-02 18:29:34 +0000

    Outline parameters

--- pack/eddy4R.maps/R/def.plot.flux.animate.R
+++ pack/eddy4R.maps/R/def.plot.flux.animate.R
@@ -1,1 +1,20 @@
-def.plot.flux.animate
+def.plot.flux.animate <- function(
+  input_folder,
+  output_file,
+  nodata_value = 0,
+  colormap = 'YlOrRd',
+  basemap_style = 'osm',
+  palette_style = 'equal',
+  alpha = 0.4,
+  legend = TRUE
+  ) {
+  
+  # Load necessary libraries
+  library(raster)
+  library(tmap)
+  library(tmaptools)
+  library(gifski)
+  
+  
+  
+}
