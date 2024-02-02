4c2006999cf81a98dbf968b175898121ad3ebf71
sambower-atmo <sbower@atmofacts.com>
2024-02-02 16:25:24 +0000

    Function to process and add each raster file to the map.

--- pack/eddy4R.maps/R/def.plot.flux.interactive.R
+++ pack/eddy4R.maps/R/def.plot.flux.interactive.R
@@ -11,6 +11,22 @@ def.plot.flux.interactive <- function(
   # List all TIFF files in the folder
   rasterFiles <- list.files(input_path, pattern = "\\.tif$", full.names = TRUE)
   
+  # Initialize Leaflet map
+  map <- leaflet() %>%
+    addProviderTiles("OpenStreetMap", group = "Street") %>%
+    addProviderTiles("Esri.WorldImagery", group="Imagery") %>%
+    addProviderTiles("CartoDB.DarkMatter", group= "Dark")
+  
+  # Function to process and add each raster file to the map
+  processAndAddRaster <- function(filePath, map) {
+    flux <- raster(filePath)
+    # Replace zeros with NA
+    flux[flux == 0] <- NA
+    # Add raster to the map
+    map <- map %>% addRasterImage(flux, group = base::basename(filePath), colors = 'YlOrRd', opacity = 0.7, layerId = base::basename(filePath))
+    return(map)
+  }
+  
   
   
   
