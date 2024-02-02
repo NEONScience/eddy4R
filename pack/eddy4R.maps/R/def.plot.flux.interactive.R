845765b4090d1035586eb6e0509dfd3a3a0b7a9f
sambower-atmo <sbower@atmofacts.com>
2024-02-02 16:00:20 +0000

    Add parameters for def.plot.flux.interactive.

---
+++ pack/eddy4R.maps/R/def.plot.flux.interactive.R
@@ -0,0 +1,11 @@
+
+
+def.plot.flux.interactive <- function(
+  inputPath,
+  outputPath,
+  nodata_value = 0,
+  basemap_Style = 'bing',
+  alpha = 0.5,
+  colormap = 'YlOrRd',
+  style = 'equal',
+  color_n = 7) {
