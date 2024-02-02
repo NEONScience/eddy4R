e6de19381a31ad8b0003c1616e9944af3dbb4d3b
sambower-atmo <sbower@atmofacts.com>
2024-02-02 18:32:09 +0000

    Read in files and start a temporary workspace for .png images.

--- pack/eddy4R.maps/R/def.plot.flux.animate.R
+++ pack/eddy4R.maps/R/def.plot.flux.animate.R
@@ -15,6 +15,18 @@ def.plot.flux.animate <- function(
   library(tmaptools)
   library(gifski)
   
+  # Ensure tmap is in plot mode
+  tmap_mode("plot")
+  
+  # Create a temporary directory for storing map images
+  temp_dir <- tempfile()
+  dir.create(temp_dir)
+  
+  # List all raster files in the directory
+  raster_files <- list.files(input_folder, pattern = "\\.tif$", full.names = TRUE)
+  
+  # Initialize a list to store file paths of individual map images
+  temp_files <- vector("character", length(raster_files))
   
   
 }
