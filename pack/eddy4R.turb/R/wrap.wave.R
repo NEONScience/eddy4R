a39d4dd492e69f4a2383be9499b05fcebafda987
amyoung01 <7317170+amyoung01@users.noreply.github.com>
2024-04-18 21:05:05 +0000

    Add zero padding option for time series but won't be default for now. Need to test impact of detrending data vs. zero-mean data.

--- pack/eddy4R.turb/R/wrap.wave.R
+++ pack/eddy4R.turb/R/wrap.wave.R
@@ -47,10 +47,11 @@
 
 wrap.wave <- function(
 dfInp,
-numScal = 15, # Defaults to 15 for NEON SAE data, max allowable with 36000 observations (2^15 = 32768)
 FuncWave = "haar", # From Nordbo and Katul 2012, orthogonal wavelet basis is ideal
 FreqSamp = 20, #Defaults to 20Hz
+zeroPad = FALSE,
 ThshMiss = 0.1,
+init = c(1, 1), # Parameter initialization for optimized curve fitting of weighted spectra
 paraStbl = NULL # Stability not currently considered
 ) {
 
@@ -70,32 +71,36 @@ if(rpt$qfMiss$veloZaxsHor == 1) invisible(lapply(names(rpt$qfMiss), function(x) 
 # fill missing values through linear interpolation
 # NAs at start and end are removed by setting na.rm = TRUE
 tmp <- zoo::na.approx(dfInp, na.rm = TRUE, rule = 2) #Rule 2 allows extrapolating end values using the nearest value or explicitly using zoo::na.locf, could be replaced with zeros using na.fill with any value: see https://stackoverflow.com/questions/7317607/interpolate-na-values-in-a-data-frame-with-na-approx
-tmpNorm <- as.data.frame(scale(tmp, center = TRUE, scale = FALSE))
+tmpNorm <- as.data.frame(scale(tmp, center = TRUE, scale = FALSE)) # Recenter time series after filling in NAs
 
 #Failsafe in case all values are NaN
 if(nrow(tmpNorm) > 1) dfInp <- tmpNorm
 
-# Test code for padding time series with zeros
-# numZero <- 2^numScal - nrow(dfInp)
-# dfInpPad <- as.data.frame(matrix(0, nrow = nrow(dfInp) + numZero, ncol = ncol(dfInp)))
-# colnames(dfInpPad) <- colnames(dfInp)
-# 
-# dfInpPad[seq(numZero / 2 + 1, numZero / 2 + nrow(dfInp)), ] <- dfInp
-# rm(dfInp) 
-#
-# dfInp <- as.data.frame(stats::ts(
-#   dfInpPad,
-#   start = 0, 
-#   frequency = FreqSamp
-# ))
+# Determine number of scales based on whether zero padding of time series will be done AND current number of observations in time series
+if (zeroPad) {
+  
+  numScal <- ceiling(log(nrow(dfInp), base = 2))
+  
+  numZero <- 2^numScal - nrow(dfInp)
+  dfInpPad <- as.data.frame(matrix(0, nrow = nrow(dfInp) + numZero, ncol = ncol(dfInp)))
+  colnames(dfInpPad) <- colnames(dfInp)
+  
+  dfInpPad[seq(numZero / 2 + 1, numZero / 2 + nrow(dfInp)), ] <- dfInp
+  dfInp <- dfInpPad
+  rm(dfInpPad)
+  
+} else {
+  
+  numScal <- floor(log(nrow(dfInp), base = 2))
+  dfInp <- dfInp[seq(1, 2^numScal), ] # only used first 2^J observations
+  
+}
 
 dfInp <- as.data.frame(stats::ts(
   dfInp,
   start = 0,
   frequency = FreqSamp
 ))
-
-dfInp <- dfInp[seq(1, 2^numScal), ] # only used first 2^J observations
 
 rpt$wave <- list()
 
