ce8ac83cfb40be42293299cf3ea3966ba2d0f1fa
ddurden <daviddurden27@gmail.com>
2025-07-02 05:37:25 +0000

    - adding an argument to remove sensor from despiking

--- pack/eddy4R.qaqc/R/wrap.qf.rmv.data.R
+++ pack/eddy4R.qaqc/R/wrap.qf.rmv.data.R
@@ -53,6 +53,7 @@
 wrap.qf.rmv.data <- function(
   inpList,
   Sens = NULL,
+  DspkSensRmv = c("amrs",NA),
   qfRmv = NULL,
   Vrbs = FALSE, 
   MethMeas = c("ecte",  "ecse")[1],
@@ -80,6 +81,11 @@ wrap.qf.rmv.data <- function(
   #Apply names to the output list
   base::names(outList) <- Sens
  
+  #Determine the sensors to run through despiking
+  if (!is.na(DspkSensRmv)){
+    Sens <- Sens[!Sens%in%DspkSensRmv]
+  }
+  
   #Despiking routine 
 base::lapply(Sens, function(x){ 
     #x <- Sens[1] #for testing
@@ -105,7 +111,7 @@ base::lapply(Sens, function(x){
         #Create flag name
         nameQf <- base::paste0("qfSpk",base::toupper(base::substring(y,1,1)),base::substring(y,2,base::nchar(y)))
         #Output despiking flag
-        outList[[x]]$qfSpk[[nameQf]] <<- tmp$qfSpk
+        outList[[x]]$qfSpk[[nameQf]] <<- base::as.integer(tmp$qfSpk)
         
       })  #End lapply for variables
   })#End lapply around sensors
@@ -117,14 +123,14 @@ base::lapply(Sens, function(x){
     #Outputting the data ffdf's
     rpt$data[[x]] <<- ff::as.ffdf(outList[[x]]$inpData) 
     rpt$data[[x]] <<- eddy4R.base::def.unit.var(samp = rpt$data[[x]], refe = inpList$data[[x]]) #Copy units
-    rpt$qfqm[[x]] <<- ff::as.ffdf(base::cbind(rpt$qfqm[[x]][], outList[[x]]$qfNull, as.data.frame(outList[[x]]$qfSpk)))
+    rpt$qfqm[[x]] <<- ifelse(x == "amrs", ff::as.ffdf(base::cbind(rpt$qfqm[[x]][], outList[[x]]$qfNull)), ff::as.ffdf(base::cbind(rpt$qfqm[[x]][], outList[[x]]$qfNull, as.data.frame(outList[[x]]$qfSpk))))
   })
   }else{
   base::lapply(base::names(outList), function(x) {
     #Outputting the data ffdf's
     rpt$data[[x]] <<- as.data.frame(outList[[x]]$inpData) 
     rpt$data[[x]] <<- eddy4R.base::def.unit.var(samp = rpt$data[[x]], refe = inpList$data[[x]]) #Copy units
-    rpt$qfqm[[x]] <<- as.data.frame(base::cbind(rpt$qfqm[[x]][], outList[[x]]$qfNull, as.data.frame(outList[[x]]$qfSpk)))
+    rpt$qfqm[[x]] <<- ifelse(x == "amrs", as.data.frame(base::cbind(rpt$qfqm[[x]][], outList[[x]]$qfNull)), as.data.frame(base::cbind(rpt$qfqm[[x]][], outList[[x]]$qfNull, as.data.frame(outList[[x]]$qfSpk))))
   })
   }#End MethFf if statment
 
